61c213f601814307de3835e58c6c124644c77935
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2000-04-01 17:20:15 +0000

    Added rd/wt_eref
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@53 29f243b5-bcef-41bb-bbe7-8e20262f3206

---
+++ src/lib/libfile30/rd_eref.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file30.h"
+#include "file30.gbl"
+#include <libciomr.h>
+
+/* file30_rd_eref(): Reads in the reference energy (if different from SCF).
+**
+**   takes no arguments.
+**
+**   returns: double eref  the reference energy.
+*/
+
+
+double file30_rd_eref(void)
+{
+  int  natom;
+  PSI_FPTR junk;
+  PSI_FPTR eref_ptr;
+  double eref;
+
+  natom = file30_rd_natom();
+  eref_ptr = (PSI_FPTR) 
+           ((info30_.mcalcs[0]+60+20+natom*6-1)*sizeof(int)+2*sizeof(double));
+
+  wreadw(info30_.filenum, (char *) &eref, (int) sizeof(double),
+	 eref_ptr, &junk);
+
+  return eref;
+}
---
+++ src/lib/libfile30/wt_eref.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file30.h"
+#include "file30.gbl"
+#include <libciomr.h>
+
+/* file30_wt_eref(): Writes the reference energy to file30.
+**
+**   arguments: double eref  the reference energy.
+**
+**   returns nothing.
+*/
+
+void file30_wt_eref(double eref)
+{
+  int  natom;
+  PSI_FPTR junk;
+  PSI_FPTR eref_ptr;
+
+  natom = file30_rd_natom();
+  eref_ptr = (PSI_FPTR) 
+           ((info30_.mcalcs[0]+60+20+natom*6-1)*sizeof(int)+2*sizeof(double));
+
+  wwritw(info30_.filenum, (char *) &eref, (int) sizeof(double),
+	 eref_ptr, &junk);
+
+  return;
+}
