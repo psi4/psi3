8e27a56e33a2f856f3fb1559c03ebc11b1022497
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2000-06-27 21:22:23 +0000

    Minor string operation fix.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@145 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cints/DFT/dft_init.c
+++ src/bin/cints/DFT/dft_init.c
@@ -48,15 +48,15 @@ void dft_init(void){
 		DFT_options.correlation_V_function = no_funct;
 	    }
 	    else
-		punt("Unrecognized fucntional specified with FUNCTIONAL keyword");
+		punt("Unrecognized fucntional specified with keyword FUNCTIONAL");
 	}
 	else
-	    punt("Must define a functional with FUNCTIONAL keyword");
+	    punt("Must define a functional with keyword FUNCTIONAL");
     }
     
     else if(elem_in_func == 2){
 	errcod = ip_string("FUNCTIONAL",&exchstring,1,0);
-	
+
         if(!strcmp(exchstring,"SLATER")){
 	    DFT_options.exchange_function = slater;
 	    DFT_options.exchange_V_function = d_slater;
@@ -66,7 +66,7 @@ void dft_init(void){
 	    DFT_options.exchange_function = density;
 	    DFT_options.exchange_V_function = density;
 	    DFT_options.den_calc = calc_density;
-	}	
+	}
 	else
 	    punt("Unrecognized or nonimplemented exchange functional specified");
 	
@@ -75,11 +75,19 @@ void dft_init(void){
 	if(!strcmp(corrstring,"NONE")){
 	    DFT_options.correlation_function = no_funct;
 	    DFT_options.correlation_V_function = no_funct;
-	    corrstring = "";
+	    corrstring[0] = '\0';
 	}
 	else
 	    punt("Unrecognized or nonimplemented correlation fuctional specified");
-	funcstring = strcat(exchstring,corrstring);
+
+	i = strlen(exchstring) + strlen(corrstring);
+	if (i == 0)
+	  funcstring = strdup("NONE");
+	else {
+	  funcstring = (char *) malloc(sizeof(char)* (i + 1));
+	  sprintf(funcstring,"%s%s",exchstring,corrstring);
+	}
+	free(exchstring); free(corrstring);
     }
     else
 	punt("Something wrong in the specification of the FUNCTIONAL keyword");
@@ -106,22 +114,26 @@ void dft_init(void){
     }
     else
 	punt("No Special Grids have been implemented in this code yet");
-   
+
+    /*------------------------------------
+      This is for debugging purposes only
+      There's no need to use separate
+      print level for DFT in the future
+     ------------------------------------*/
     /* Print out DFT information? */
-    
     DFT_options.prtflag = 0;
     errcod = ip_data("DFT_PRINT","%d",&(DFT_options.prtflag),0);
     
     if(DFT_options.prtflag > 0){
-	fprintf(outfile,"\nDFT intialization info\n");
-	fprintf(outfile,"\nFunctional = %s",funcstring);
+	fprintf(outfile," -DFT info:\n");
+	fprintf(outfile,"    Functional = %s",funcstring);
 	for(i=0;i<DFT_options.grid_dim;i++){
-	    fprintf(outfile,"\n%s",DFT_options.grid_info[0].grid_label);
-	    fprintf(outfile,"\nRadial Cutoff = %e"
-		    ,DFT_options.grid_info[i].rcut);
-	    fprintf(outfile,"\nRadial Points = %5d Angular Points =%5d"
-		    ,DFT_options.rpoints
-		    ,DFT_options.grid_info[i].angpoints);
+	    fprintf(outfile,"   %s",DFT_options.grid_info[0].grid_label);
+	    fprintf(outfile," Radial Cutoff = %e",
+		    DFT_options.grid_info[i].rcut);
+	    fprintf(outfile," Radial Points = %5d Angular Points =%5d\n",
+		    DFT_options.rpoints,
+		    DFT_options.grid_info[i].angpoints);
 	}
 	fflush(outfile);
     }
