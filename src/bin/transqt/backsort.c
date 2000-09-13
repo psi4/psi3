78286742f6f26cf41fe3e7d2f7547f8820aa5a4b
psi <psi@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2000-09-13 17:02:36 +0000

    Included "forgotten" stdlib.h so that compiles fine in 64-bit environments.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@254 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/transqt/backsort.c
+++ src/bin/transqt/backsort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <psifiles.h>
 #include <libciomr.h>
 #include <iwl.h>
--- src/bin/transqt/yoshimine.c
+++ src/bin/transqt/yoshimine.c
@@ -12,6 +12,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <libciomr.h>
 #include <qt.h>
