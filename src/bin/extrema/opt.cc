414d2ca4e1227871f9d26f63bdbcc2232b81b68f
kenny <kenny@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2000-10-26 01:09:20 +0000

    *** empty log message ***
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@292 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/extrema/opt.cc
+++ src/bin/extrema/opt.cc
@@ -12,185 +12,69 @@
 extern "C" {
 #include <libciomr.h>
 #include <ip_libv1.h>
-#include <file30.h>
 #include <physconst.h>
-}
+} 
 
-#include "simple_internal.h"
-#include "coord_base.h"
-#include "z_class.h"
 #include "opt.h"
 
-void form_B(struct z_class& z);
-void back_transform(struct z_class& z);
+void input();
+void read_file11(coord_base* coord);
 
 void main() {
 
-  int i, j;
-  double *cgrad_vec,*cart_vec, coord;
-  FILE *temp;
-  
-  cart_vec = init_array(3*num_atoms);
+  input(); 
 
-  /*set up i/o stuff*/
-  ffile(&infile,"input.dat",2);
-  ffile(&outfile,"output.dat",1);
- 
-  ip_set_uppercase(1);
-  ip_initialize(infile,outfile);
-  ip_cwk_clear();
-  ip_cwk_add(":INPUT");
-  
-  /*read in cartesian coordinates and gradients from file11*/
-  read_file11();
-  for(i=0;i<num_atoms;++i){
-      for(j=0;j<3;++j){
-	  cart_geom[i][j] = cart_geom[i][j] /  _bohr2angstroms;
-      }
-  }
+  typedef z_class type;
+  if(coord_type!=0) 
+      punt("Extrema currently capable of z-matrix optimization only");
+  type coord_set;
 
-  int spot=0;
-  for(i=0;i<num_atoms;++i) {
-      for(j=0;j<3;++j) {
-	  cart_vec[spot]=cart_geom[i][j];
-	  ++spot;
-      }
-  }
+  /*read previous iteration info from opt.dat*/
+  iteration = coord_set.read_opt();
+  fprintf(outfile,"\nStarting Optimization Iteration %d\n",iteration); 
 
-  /*if using z-matrix construct z_class*/
-  if(ip_exist("ZMAT",0)){
+  /*get cartesian coordinates and gradients*/
+  read_file11(dynamic_cast<coord_base*>(&coord_set));
+  coord_set.print_carts();
+  coord_set.print_c_grads();
 
-    if( num_atoms==2 )
-	num_coords = 1;
-    else if( num_atoms>2 )
-	num_coords = 3*num_atoms - 6;
-    else punt("Bad number of atoms or trouble reading from input.dat"); 
+  /*compute B*/
+  coord_set.compute_B();
+  coord_set.print_B();
 
-    /*allocate memory*/
-    global_allocate();
+  /*transform gradients from cartesian to internal coordinates*/
+  coord_set.grad_trans();
 
-    z_class z_coord(num_coords);
-    ip_done();
+  /*form or update H*/
+  coord_set.update_H();
 
-    fflush(outfile);
-    B_mat = (double **) malloc(num_coords*sizeof(double *));
-    form_B(z_coord);
-    fprintf(outfile,"\nB_mat:\n");
-    print_mat(B_mat,num_coords,3*num_atoms,outfile);
+  coord_set.opt_step();
+  coord_set.back_transform(); 
 
-    /*transform gradients to internal coordinates*/
-    cgrad_vec = init_array(3*num_atoms);
- 
-    int pos=0;
-    for (i=0;i<num_atoms;++i) {
-        for(j=0;j<3;++j) {
-            cgrad_vec[pos] = cart_grad[i][j];
-            ++pos;
-          }
-      }
- 
-    for (i=0;i<num_coords;++i) {
-        for (j=0;j<(3*num_atoms);++j) {
-            grad_vec[i] += B_mat[i][j]*cgrad_vec[j];
-          }
-      }
- 
-    fprintf(outfile,"\ngradient vector in cartesian coordinates:\n");
-    for (i=0;i<3*num_atoms;++i) {
-        fprintf(outfile,"%lf\n",cgrad_vec[i]);
-      }
- 
-    fprintf(outfile,"\ngradient vector in internal coordinates:\n");
-    for (i=0;i<num_coords;++i) {
-        fprintf(outfile,"%lf\n",grad_vec[i]);
-      }
+  coord_set.write_opt();
+  coord_set.write_file30();
 
-    free(cgrad_vec);
-
-    iteration = read_opt();
-
-    fprintf(outfile,"\niteration: %d\n",iteration);
-    old_coord_vec = init_array(num_coords);    
-
-    if(iteration == 1) {
- 
-        for(i=0;i<num_coords;++i) {
-	    if( z_coord.coord_arr[i].get_type() == 0 ){
-	        H[i][i] = 1.0;
-		coord_vec[i] = old_coord_vec[i] = z_coord.coord_arr[i].get_val() * _bohr2angstroms;
-	    }
-	    if( z_coord.coord_arr[i].get_type() == 1 || z_coord.coord_arr[i].get_type() == 2 ){
-	        H[i][i] = 4.0;
-		coord_vec[i] = old_coord_vec[i] = z_coord.coord_arr[i].get_val() * _pi/180.0;
-	    }
-        }
- 
-        fprintf(outfile,"\nForming empirical Hessian:\n");
-        print_mat(H,num_coords,num_coords,outfile);
-    }
-
-    else {
-        for(i=0;i<num_coords;++i) {
-	    coord_vec[i] = old_coord_vec[i] = z_coord.coord_arr[i].get_val()*_bohr2angstroms;
-        }
-        update_H();
-    }
-
-    write_opt();
-
-    /*compute optimization step*/
-    opt_step();
-
-    fprintf(outfile,"\nnew coordinate values:\n");
-    fprintf(outfile,"%lf\n",coord_vec[0]);
-    fprintf(outfile,"%lf\n",coord_vec[1]);
-    fprintf(outfile,"%lf\n",coord_vec[2]);
-
-
-    /*update z_geom with new coordinates*/
-    j=0;
-    for(i=1;i<num_atoms;++i) {
-        if(i==1) {
-	    z_coord.z_geom[i].bond_val = coord_vec[j];
-	    ++j;
-        }
-        if(i==2) {
-	    z_coord.z_geom[i].bond_val = coord_vec[j];
-	    ++j; 
-	    z_coord.z_geom[i].angle_val = coord_vec[j];
-	    ++j; 
-        }
-        else if(i>2) {
-	    z_coord.z_geom[i].bond_val = coord_vec[j];
-	    ++j;
-	    z_coord.z_geom[i].angle_val = coord_vec[j];
-	    ++j;
-	    z_coord.z_geom[i].tors_val - coord_vec[j];
-	    ++j;
-        }
-    }
-
-    /*test z_to_cart*/
-    //double **cart_geom;
-    //cart_geom = file30_z_to_cart(z_coord.z_geom,num_atoms);
-    //print_mat(cart_geom,num_atoms,3,outfile);
-
-    /*need to use iterative transform for now*/
-    back_transform(z_coord);
-    fprintf(outfile,"\nnew cartesian coordinates\n");
-    print_mat(cart_geom,num_atoms,3,outfile);
-
-    file30_wt_geom(cart_geom);
-    file30_wt_zmat(z_coord.z_geom,num_atoms);
-      
-  }
-  else punt("z-matrix not found in input ... can't do whatever it is you want to do yet");
-
-  global_free();
   printf("\nNormal termination\n");
   exit(0);
-  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/bin/extrema/small_functions.cc
+++ src/bin/extrema/small_functions.cc
@@ -11,6 +11,7 @@
 
 extern "C" {
   #include <libciomr.h>
+  #include <ip_libv1.h>
 }
 
 #define EXTERN
@@ -35,31 +36,7 @@ void punt(char *mess)
   exit(1);
 }
 
-void global_allocate() {
- 
-  coord_vec = init_array(num_coords);
-  old_coord_vec = init_array(num_coords);
-  grad_vec = init_array(num_coords);
-  H = init_matrix(num_coords,num_coords);  
-  coord_old = init_array(num_coords);
-  grad_old = init_array(num_coords);
-  H_old = init_matrix(num_coords,num_coords);
- 
-  return;
-}
- 
-void global_free() {
- 
-  free(coord_vec);
-  free(grad_vec);
-  free_matrix(H,num_coords);
-  free(coord_old);
-  free(grad_old);
-  free_matrix(H_old,num_coords);
- 
-  return;
-}           
-                    
+
 
 double dot_pdt(double *vec1, double *vec2, int num) {
 
@@ -75,4 +52,65 @@ double dot_pdt(double *vec1, double *vec2, int num) {
 
 
 
+/*----------------------------------------------------------------------------
+
+       **SYM_MATRIX_INVERT
+
+       inverts a matrix by diagonalization
+
+       parameters:
+             **A = matrix to be inverted
+             dim = dimension of A
+             print_det = print determinant if 1, nothing if 0
+             redundant = zero eigenvalues allowed if 1
+
+       returns:
+             **inv_A = inverse of A
+
+                                                      written by Rollin King
+----------------------------------------------------------------------------*/
+
+double **symm_matrix_invert(double **A, int dim, int print_det, int redundant) {
+  int i;
+  double **A_inv, **A_vects, *A_vals, **A_temp, det=1.0;
+
+  A_inv   = init_matrix(dim,dim);
+  A_temp  = init_matrix(dim,dim);
+  A_vects = init_matrix(dim,dim);
+  A_vals  = init_array(dim);
+
+  sq_rsp(dim,dim,A,A_vals,1,A_vects,1E-10);
+
+  if (redundant == 0) {
+     for (i=0;i<dim;++i) {
+        det *= A_vals[i];
+        A_inv[i][i] = 1.0/A_vals[i];
+     }
+     if (print_det)
+        fprintf(outfile,"Determinant: %10.6e\n",det);
+     if (fabs(det) < 1E-10) {
+        fprintf(outfile,"Determinant: %10.6e\n",det);
+        fprintf(outfile,"Determinant is too small...aborting.\n");
+        fclose(outfile);
+        exit(2);
+     }
+  }
+  else {
+     for (i=0;i<dim;++i) {
+        det *= A_vals[i];
+        if (fabs(A_vals[i]) > 1E-10)
+           A_inv[i][i] = 1.0/A_vals[i];
+     }
+     if (print_det)
+        fprintf(outfile,"Determinant: %10.6e\n",det);
+  }
+
+  mmult(A_inv,0,A_vects,1,A_temp,0,dim,dim,dim,0);
+  mmult(A_vects,0,A_temp,0,A_inv,0,dim,dim,dim,0);
+
+  free(A_vals);
+  free_matrix(A_vects,dim);
+  free_matrix(A_temp,dim);
+  return A_inv;
+}
 
