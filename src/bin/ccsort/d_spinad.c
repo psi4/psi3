e7334769730badc4d238054c4dec979ea803568e
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2000-12-08 20:38:03 +0000

    Forgot to add the D-integral spin-adaptation code. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@313 29f243b5-bcef-41bb-bbe7-8e20262f3206

---
+++ src/bin/ccsort/d_spinad.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <libciomr.h>
+#include <dpd.h>
+#define EXTERN
+#include "globals.h"
+
+void d_spinad(void)
+{
+  dpdbuf4 D, D1;
+
+  dpd_buf4_init(&D, CC_DINTS, 0, 0, 5, 0, 5, 0, "D <ij|ab>");
+  dpd_buf4_copy(&D, CC_DINTS, "D 2<ij|ab> - <ji|ab>");
+  dpd_buf4_sort(&D, CC_TMP0, qprs, 0, 5, "D <ji|ab>");
+  dpd_buf4_close(&D);
+
+  dpd_buf4_init(&D, CC_DINTS, 0, 0, 5, 0, 5, 0, "D 2<ij|ab> - <ji|ab>");
+  dpd_buf4_scm(&D, 2.0);
+  dpd_buf4_init(&D1, CC_TMP0, 0, 0, 5, 0, 5, 0, "D <ji|ab>");
+  dpd_buf4_axpy(&D1, &D, -1);
+  dpd_buf4_close(&D1);
+  dpd_buf4_close(&D);
+}
