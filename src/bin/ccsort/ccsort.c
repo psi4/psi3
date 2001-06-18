a9a6182501bef51a0b3db4c57cb83c91d49f0ae9
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2001-06-18 21:56:01 +0000

    Adding UHF references to ccsort.  Current version is incomplete. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@401 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccsort/ccsort.c
+++ src/bin/ccsort/ccsort.c
@@ -52,9 +52,16 @@ int main(int argc, char *argv[])
   cachefiles = init_int_array(PSIO_MAXUNIT);
   cachelist = cacheprep(params.cachelev, cachefiles);
 
-  dpd_init(0, moinfo.nirreps, params.memory, 0, cachefiles, cachelist, 
-           NULL, 2, moinfo.occpi, moinfo.occ_sym, moinfo.virtpi, 
-           moinfo.vir_sym);
+  if(params.ref == 2) { /*** UHF references ***/
+    dpd_init(0, moinfo.nirreps, params.memory, 0, cachefiles, cachelist, 
+	     NULL, 4, moinfo.aoccpi, moinfo.aocc_sym, moinfo.boccpi, moinfo.bocc_sym,
+	     moinfo.avirtpi, moinfo.avir_sym, moinfo.bvirtpi, moinfo.bvir_sym);
+  } 
+  else { /*** RHF/ROHF references ***/
+    dpd_init(0, moinfo.nirreps, params.memory, 0, cachefiles, cachelist, 
+	     NULL, 2, moinfo.occpi, moinfo.occ_sym, moinfo.virtpi, 
+	     moinfo.vir_sym);
+  }
 
   sort_oei();
   sort_tei();
@@ -152,35 +159,95 @@ void cleanup(void)
   free(moinfo.fruocc);
   free(moinfo.frdocc);
   for(i=0; i < moinfo.nirreps; i++)
-      free(moinfo.labels[i]);
+    free(moinfo.labels[i]);
   free(moinfo.labels);
-  free(moinfo.occ);
-  free(moinfo.vir);
-  free(moinfo.socc);
-  free(moinfo.all_occ);
-  free(moinfo.all_vir);
-  free(moinfo.all_socc);
+
+  if(params.ref == 2) {
+    free(moinfo.aocc);
+    free(moinfo.bocc);
+    free(moinfo.avir);
+    free(moinfo.bvir);
+    free(moinfo.all_aocc);
+    free(moinfo.all_bocc);
+    free(moinfo.all_avir);
+    free(moinfo.all_bvir);
+    free(moinfo.aoccpi);
+    free(moinfo.boccpi);
+    free(moinfo.avirtpi);
+    free(moinfo.bvirtpi);
+    free(moinfo.all_aoccpi);
+    free(moinfo.all_boccpi);
+    free(moinfo.all_avirtpi);
+    free(moinfo.all_bvirtpi);
+
+    free(moinfo.cc_aocc);
+    free(moinfo.cc_bocc);
+    free(moinfo.cc_avir);
+    free(moinfo.cc_bvir);
+    free(moinfo.qt_aocc);
+    free(moinfo.qt_bocc);
+    free(moinfo.qt_avir);
+    free(moinfo.qt_bvir);
+    free(moinfo.aocc_sym);
+    free(moinfo.bocc_sym);
+    free(moinfo.avir_sym);
+    free(moinfo.bvir_sym);
+
+    free(moinfo.cc_allaocc);
+    free(moinfo.cc_allbocc);
+    free(moinfo.cc_allavir);
+    free(moinfo.cc_allbvir);
+    free(moinfo.qt_allaocc);
+    free(moinfo.qt_allbocc);
+    free(moinfo.qt_allavir);
+    free(moinfo.qt_allbvir);
+    free(moinfo.allaocc_sym);  
+    free(moinfo.allbocc_sym);  
+    free(moinfo.allavir_sym);
+    free(moinfo.allbvir_sym);
+
+    free(moinfo.aocc_off);
+    free(moinfo.bocc_off);
+    free(moinfo.avir_off);
+    free(moinfo.bvir_off);
+    free(moinfo.all_aocc_off);
+    free(moinfo.all_bocc_off);
+    free(moinfo.all_avir_off);
+    free(moinfo.all_bvir_off);
+  }
+  else {
+    free(moinfo.occ);
+    free(moinfo.vir);
+    free(moinfo.all_occ);
+    free(moinfo.all_vir);
+    free(moinfo.socc);
+    free(moinfo.all_socc);
+    free(moinfo.occpi);
+    free(moinfo.virtpi);
+    free(moinfo.all_occpi);
+    free(moinfo.all_virtpi);
+
+    free(moinfo.cc_occ);
+    free(moinfo.cc_vir);
+    free(moinfo.qt_occ);
+    free(moinfo.qt_vir);
+    free(moinfo.occ_sym);
+    free(moinfo.vir_sym);
+
+    free(moinfo.cc_allocc);
+    free(moinfo.cc_allvir);
+    free(moinfo.qt_allocc);
+    free(moinfo.qt_allvir);
+    free(moinfo.allocc_sym);  
+    free(moinfo.allvir_sym);
+
+    free(moinfo.occ_off);
+    free(moinfo.vir_off);
+    free(moinfo.all_occ_off);
+    free(moinfo.all_vir_off);
+  }
+
   free(moinfo.frozen);
-  free(moinfo.cc_occ);
-  free(moinfo.cc_vir);
-  free(moinfo.qt_occ);
-  free(moinfo.qt_vir);
-  free(moinfo.occ_sym);
-  free(moinfo.vir_sym);
-  free(moinfo.cc_allocc);
-  free(moinfo.cc_allvir);
-  free(moinfo.qt_allocc);
-  free(moinfo.qt_allvir);
-  free(moinfo.allocc_sym);
-  free(moinfo.allvir_sym);
-  free(moinfo.occ_off);
-  free(moinfo.vir_off);
-  free(moinfo.all_occ_off);
-  free(moinfo.all_vir_off);
-  free(moinfo.occpi);
-  free(moinfo.virtpi);
-  free(moinfo.all_occpi);
-  free(moinfo.all_virtpi);
 
   free(ioff);
 
