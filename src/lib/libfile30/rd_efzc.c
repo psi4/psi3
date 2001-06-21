25751b208e71af1baef3bb3f41326ba39095233e
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2001-06-21 18:38:36 +0000

    Adding functions to read/write the frozen-core energy from/to file30. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@406 29f243b5-bcef-41bb-bbe7-8e20262f3206

---
+++ src/lib/libfile30/rd_efzc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file30.h"
+#include "file30.gbl"
+#include <libciomr.h>
+
+/* file30_rd_efzc(): Reads in the frozen-core energy.
+**
+**   takes no arguments.
+**
+**   returns: double efzc  the frozen-core energy.
+*/
+
+
+double file30_rd_efzc(void)
+{
+  int  natom;
+  PSI_FPTR junk;
+  PSI_FPTR ptr;
+  double efzc;
+
+  natom = file30_rd_natom();
+  ptr = (PSI_FPTR) 
+           ((info30_.mcalcs[0]+60+20+natom*6-1)*sizeof(int)+4*sizeof(double));
+
+  wreadw(info30_.filenum, (char *) &efzc, (int) sizeof(double),
+	 ptr, &junk);
+
+  return efzc;
+}
