9a0982975f1f4cf51fb942e91c8452040eb709d7
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2001-06-21 18:39:08 +0000

    Added some new distributions to c_sort.c -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@407 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccsort/c_sort.c
+++ src/bin/ccsort/c_sort.c
@@ -25,6 +25,11 @@ void c_sort(void)
     dpd_buf4_close(&D);
     dpd_buf4_close(&C);
 
+    /* <IA||JB> (IA,BJ) (Wmbej.c) */
+    dpd_buf4_init(&C, CC_CINTS, 0, 20, 20, 20, 20, 0, "C <IA||JB>");
+    dpd_buf4_sort(&C, CC_CINTS, pqsr, 20, 21, "C <IA||JB> (IA,BJ)");
+    dpd_buf4_close(&C);
+
     /*** BB ***/
     dpd_buf4_init(&C, CC_CINTS, 0, 30, 30, 30, 30, 0, "C <ia|jb>");
     dpd_buf4_copy(&C, CC_CINTS, "C <ia||jb>");
@@ -38,6 +43,12 @@ void c_sort(void)
     dpd_buf4_axpy(&D, &C, -1);
     dpd_buf4_close(&D);
     dpd_buf4_close(&C);
+
+    /* <ia||jb> (ia,bj) (Wmbej.c) */
+    dpd_buf4_init(&C, CC_CINTS, 0, 30, 30, 30, 30, 0, "C <ia||jb>");
+    dpd_buf4_sort(&C, CC_CINTS, pqsr, 30, 31, "C <ia||jb> (ia,bj)");
+    dpd_buf4_close(&C);
+
   }
   else {
     /* <ia||jb> = <ia|jb> - <ia|bj> = <ia|jb> - <ij|ba> */
@@ -69,7 +80,7 @@ void c_sort(void)
     dpd_buf4_sort(&C, CC_CINTS, pqsr, 10, 11, "C <ia|jb> (ia,bj)");
     dpd_buf4_close(&C);
 
-    /* <ia||jb> (ia,bj) */
+    /* <ia||jb> (ia,bj) (Wmbej.c) */
     dpd_buf4_init(&C, CC_CINTS, 0, 10, 10, 10, 10, 0, "C <ia||jb>");
     dpd_buf4_sort(&C, CC_CINTS, pqsr, 10, 11, "C <ia||jb> (ia,bj)");
     dpd_buf4_close(&C);
