596d7e3eca303861b3c7224ef24fc78608cafaec
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2001-06-21 20:50:02 +0000

    Added function to filter frozen orbital indices from a packed list of one-electron integrals. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@408 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libqt/qt.h
+++ src/lib/libqt/qt.h
@@ -71,3 +71,5 @@ void timer_init(void);
 void timer_done(void);
 void timer_on(char *key);
 void timer_off(char *key);
+
+void filter(double *input, double *output, int *ioff, int norbs, int nfzc, int nfzv);
