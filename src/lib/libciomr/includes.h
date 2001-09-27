a2b8676e27b271323f3fe65ddc8fc807109d4dc7
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2001-09-27 11:58:09 +0000

    Minor changes to cpp directives in includes.h. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@502 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libciomr/includes.h
+++ src/lib/libciomr/includes.h
@@ -6,14 +6,7 @@
 #include <math.h>
 #include "libciomr.h"
 
-#if defined(DEC)
-#  include <sys/types.h>
-#  include <time.h>
-#  include <sys/times.h>
-#  include <sys/param.h>
-#  include <sys/mount.h>
-#  include <sys/stat.h>
-#elif defined(SGI)
+#if defined(SGI)
 #  include <sys/stat.h>
 #  include <sys/types.h>
 #  include <sys/times.h>
@@ -32,10 +25,12 @@
 #  include <sys/times.h>
 #  include <sys/param.h>
 #else
-#  include <sys/stat.h>
-#  include <sys/vfs.h>
+#  include <sys/types.h>
 #  include <time.h>
 #  include <sys/times.h>
+#  include <sys/param.h>
+#  include <sys/mount.h>
+#  include <sys/stat.h>
 #endif
 
 #if (defined(DEC)||defined(SUN))
@@ -47,13 +42,3 @@
 #define DSIGN(a,b) ((b) >= 0.0) ? (fabs(a)) : (-fabs(a))
 #define MIN0(a,b) ((a)<(b)) ? (a) : (b)
 #define MAX0(a,b) ((a)>(b)) ? (a) : (b)
-
-#ifdef AIX
-# define MALLOC malloc
-#else
-# define MALLOC malloc
-#endif
-
-#ifdef USE_DMALLOC
-#include <dmalloc.h>
-#endif
