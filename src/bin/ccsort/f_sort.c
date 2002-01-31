9ab565b0d6aa276c5bc049d2b0d4a04ce59aad05
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2002-01-31 13:27:29 +0000

    Removed a redundant sort for RHF/ROHF cases. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@661 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccsort/f_sort.c
+++ src/bin/ccsort/f_sort.c
@@ -13,15 +13,4 @@ void f_sort(void)
     dpd_buf4_sort_ooc(&F, CC_FINTS, spqr, 27, 29, "F <iA|bC>");
     dpd_buf4_close(&F);
   }
-  else { /*** RHF/ROHF ***/
-    /* <ia||bc> */
-    dpd_buf4_init(&F, CC_FINTS, 0, 10, 7, 10, 5, 1, "F <ia|bc>");
-    dpd_buf4_copy(&F, CC_FINTS, "F <ia||bc> (ia,b>c)");
-    dpd_buf4_close(&F);
-
-    /* <ai|bc> */
-    dpd_buf4_init(&F, CC_FINTS, 0, 10, 5, 10, 5, 0, "F <ia|bc>");
-    dpd_buf4_sort_ooc(&F, CC_FINTS, qpsr, 11, 5, "F <ai|bc>");
-    dpd_buf4_close(&F);
-  }
 }
