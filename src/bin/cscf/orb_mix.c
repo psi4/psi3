624035498f09a53817251f1578de76f99870b114
sherrill <sherrill@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2002-07-10 23:04:56 +0000

    Clean up some junk in orb_mix.c ---CDS
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@952 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cscf/orb_mix.c
+++ src/bin/cscf/orb_mix.c
@@ -35,95 +35,95 @@ void orb_mix(void)
  
     fprintf(outfile, "\n  ***** Orbital Mixing being used to find unique UHF solution *****\n");
 
-    /* figure out which irrep has the HOMO for each spin case (0 and 1) */
+    /* figure out which irrep has the HOMO for alpha spin */
+    spin = 0;
 
-    for (spin=0; spin<2; spin++) {
-      for (irr=0,found_guess=0; irr<num_ir; irr++) {
-        if (spin_info[spin].scf_spin[irr].noccup > 0) {
-          tval = spin_info[spin].scf_spin[irr].fock_evals[spin_info[spin].scf_spin[irr].noccup-1]; 
-          if (!found_guess) {
+    /* these guess eigenvalues are really awful but it seems ok
+    for (irr=0; irr<num_ir; irr++) {
+      fprintf(outfile, "Irrep %d\n", irr);
+      for (i=0; i<scf_info[irr].num_mo; i++) {
+        fprintf(outfile, "%lf\n", spin_info[spin].scf_spin[irr].fock_evals[i]);
+      }
+    }
+    */
+
+    for (irr=0,found_guess=0; irr<num_ir; irr++) {
+      if (spin_info[spin].scf_spin[irr].noccup > 0) {
+        tval = spin_info[spin].scf_spin[irr].fock_evals[spin_info[spin].scf_spin[irr].noccup-1]; 
+        if (!found_guess) {
+          homo_irrep[spin] = irr;
+          homo_energy[spin] = tval; 
+          found_guess = 1;
+        }
+        else {
+          if (tval > homo_energy[spin]) {
+            homo_energy[spin] = tval;
             homo_irrep[spin] = irr;
-            homo_energy[spin] = tval; 
-            found_guess = 1;
           }
-          else {
-            if (tval > homo_energy[spin]) {
-              homo_energy[spin] = tval;
-              homo_irrep[spin] = irr;
-            }
-          }
-        } 
-      } /* end loop over irreps */
-
-      if (!found_guess) {
-        fprintf(outfile, 
-                "cscf: (orb_mix): Can't find a valid guess for HOMO irrep!\n");
-        exit(1);
+        }
       } 
+    } /* end loop over irreps */
 
-    } /* end loop over spin cases */
-    
+    if (!found_guess) {
+      fprintf(outfile, 
+              "cscf: (orb_mix): Can't find a valid guess for HOMO irrep!\n");
+      exit(1);
+    } 
+
 
     /* now try to figure out where the LUMO is */
 
-    for (spin=0; spin<2; spin++) {
-      for (irr=0,found_guess=0; irr<num_ir; irr++) {
+    for (irr=0,found_guess=0; irr<num_ir; irr++) {
 
-        /* if there are any virtuals in this irrep... */
-        if (scf_info[irr].num_mo > spin_info[spin].scf_spin[irr].noccup) {
+      /* if there are any virtuals in this irrep... */
+      if (scf_info[irr].num_mo > spin_info[spin].scf_spin[irr].noccup) {
 
-          tval = spin_info[spin].scf_spin[irr].fock_evals[spin_info[spin].scf_spin[irr].noccup]; 
-          if (!found_guess) {
+        tval = spin_info[spin].scf_spin[irr].fock_evals[spin_info[spin].scf_spin[irr].noccup]; 
+        if (!found_guess) {
+          lumo_irrep[spin] = irr;
+          lumo_energy[spin] = tval; 
+          found_guess = 1;
+        }
+        else {
+          if (tval < lumo_energy[spin]) {
+            lumo_energy[spin] = tval;
             lumo_irrep[spin] = irr;
-            lumo_energy[spin] = tval; 
-            found_guess = 1;
           }
-          else {
-            if (tval < lumo_energy[spin]) {
-              lumo_energy[spin] = tval;
-              lumo_irrep[spin] = irr;
-            }
-          }
-        } 
-      } /* end loop over irreps */
+        }
+      }
+    } /* end loop over irreps */
 
-      if (!found_guess) {
-        fprintf(outfile, 
-                "cscf: (orb_mix): Can't find a valid guess for LUMO irrep!\n");
-        exit(1);
-      } 
-
-    } /* end loop over spin cases */
-    
+    if (!found_guess) {
+      fprintf(outfile, 
+              "cscf: (orb_mix): Can't find a valid guess for LUMO irrep!\n");
+      exit(1);
+    } 
 
 
     if (print > 1) {
-      fprintf(outfile, "Identified alpha HOMO as irrep %d, energy %12.6lf\n",
-              homo_irrep[0], homo_energy[0]);
-      fprintf(outfile, "Identified beta  HOMO as irrep %d, energy %12.6lf\n",
-              homo_irrep[1], homo_energy[1]);
-      fprintf(outfile, "Identified alpha LUMO as irrep %d, energy %12.6lf\n",
-              lumo_irrep[0], lumo_energy[0]);
-      fprintf(outfile, "Identified beta  LUMO as irrep %d, energy %12.6lf\n",
-              lumo_irrep[1], lumo_energy[1]);
+      fprintf(outfile, "Identified alpha HOMO as irrep %s, energy %12.6lf\n",
+              scf_info[homo_irrep[0]].irrep_label, homo_energy[0]);
+      fprintf(outfile, "Identified alpha LUMO as irrep %s, energy %12.6lf\n",
+              scf_info[lumo_irrep[0]].irrep_label, lumo_energy[0]);
     }
 
 
-    /* now make sure HOMO and LUMO have the same irrep! */
-    for (spin=0; spin<2; spin++) {
-      if (homo_irrep[spin] != lumo_irrep[spin]) {
-        fprintf(outfile, 
-                "cscf: (orb_mix): %s HOMO and LUMO different irreps!\n",
-                 spin == 0 ? "alpha" : "beta");
-        fprintf(outfile, "Identified HOMO as irrep %d, energy %12.6lf\n",
-              homo_irrep[spin], homo_energy[spin]);
-        fprintf(outfile, "Identified LUMO as irrep %d, energy %12.6lf\n",
-              lumo_irrep[spin], lumo_energy[spin]);
-        fprintf(outfile, "\tTry running in a subgroup of the current group\n");
-        exit(1);
-      }
+    /* now see if HOMO and LUMO have the same irrep! */
+    if (homo_irrep[spin] != lumo_irrep[spin]) {
+      fprintf(outfile, "Identified HOMO as irrep %s, energy %12.6lf\n",
+            scf_info[homo_irrep[spin]].irrep_label, homo_energy[spin]);
+      fprintf(outfile, "Identified LUMO as irrep %s, energy %12.6lf\n",
+            scf_info[lumo_irrep[spin]].irrep_label, lumo_energy[spin]);
+      fprintf(outfile, "\tHOMO and LUMO have different irreps.\n");
+      fprintf(outfile, 
+        "\tWill use lowest unoccupied from irrep %s instead\n", 
+        scf_info[homo_irrep[spin]].irrep_label);
+        lumo_irrep[spin] = homo_irrep[spin];
     }
 
+    /* now do the same for beta as for alpha */
+    homo_irrep[1] = homo_irrep[0];
+    lumo_irrep[1] = lumo_irrep[0];
 
     if (print > 4) {
       for (spin=0; spin<2; spin++) {
