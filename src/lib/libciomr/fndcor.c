559455528f43a37e0a9b250848af504236da8567
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-01-02 21:14:42 +0000

    Modified fndcor function so that it will return a default value of 256 MB if the "MEMORY" keyword is omitted from user input. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1339 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libciomr/fndcor.c
+++ src/lib/libciomr/fndcor.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <libipv1/ip_lib.h>
 
-#define DEF_MAXCRR 100000    /* default maxcor in doubles: used only if
+#define DEF_MAXCRR 32000000  /* default maxcor in doubles: used only if
                               * it can't be read in */
 
 static void fndcor_abort();
@@ -28,6 +28,10 @@ static void fndcor_abort();
 ** David Sherrill, February 1994
 ** Revised to handle more than 2GB of memory by Ed Valeev, October 2000
 **
+** Revised to return the default if memory keyword is missing and
+** raised the default to 256 MB.  
+** TDC, January 2003
+**
 ** \ingroup (CIOMR)
 */
 
@@ -41,52 +45,54 @@ void fndcor(long int *maxcrb, FILE *infile, FILE *outfile)
   double size ;
   int errcod ;
 
-   maxcrr = DEF_MAXCRR ;  /* set maxcor to default first */
+  maxcrr = DEF_MAXCRR ;  /* set maxcor to default first */
 
-   errcod = ip_count("MEMORY", &count, 0) ;
-   if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
-   else if (errcod == IPE_NOT_AN_ARRAY) { /* Scalar specification of MEMORY */
+  if(ip_exist("MEMORY",0)) { /* check if the keyword exists */
+    errcod = ip_count("MEMORY", &count, 0) ;
+    if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
+    else if (errcod == IPE_NOT_AN_ARRAY) { /* Scalar specification of MEMORY */
       errcod = ip_string("MEMORY", &maxcrr_str, 0);
       if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
       maxcrr = atol(maxcrr_str);
-   }
-   /* Array specification of MEMORY */
-   else if (count == 1) {
+    }
+    /* Array specification of MEMORY */
+    else if (count == 1) {
       errcod = ip_string("MEMORY", &maxcrr_str, 0);
       if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
       maxcrr = atol(maxcrr_str);
-   }
-   else if (count == 2) {
+    }
+    else if (count == 2) {
       errcod = ip_data("MEMORY", "%lf", &size, 1, 0) ;
       if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
       errcod = ip_data("MEMORY", "%s", type, 1, 1) ;
       if (errcod != IPE_OK) fndcor_abort(infile, outfile) ;
       /* convert string to uppercase */
       for (s=type; *s!='\0'; s++) {
-         if (*s>='a' && *s <='z') *s = *s + 'A' - 'a';
+	if (*s>='a' && *s <='z') *s = *s + 'A' - 'a';
       }
       if ((strcmp(type, "R")==0) || (strcmp(type, "REAL")==0))
-         maxcrr = (long int) size ;
+	maxcrr = (long int) size ;
       else if ((strcmp(type, "I")==0) || (strcmp(type, "INTEGER")==0)) 
-         maxcrr = (long int) (size * sizeof(int) / sizeof(double)) ;
+	maxcrr = (long int) (size * sizeof(int) / sizeof(double)) ;
       else if ((strcmp(type, "B")==0) || (strcmp(type, "BYTES")==0)) 
-         maxcrr = (long int) (size / sizeof(double)) ;
+	maxcrr = (long int) (size / sizeof(double)) ;
       else if ((strcmp(type, "KB")==0) || (strcmp(type, "KBYTES")==0)) 
-         maxcrr = (long int) (1000.0 * size / sizeof(double)) ;
+	maxcrr = (long int) (1000.0 * size / sizeof(double)) ;
       else if ((strcmp(type, "MB")==0) || (strcmp(type, "MBYTES")==0))
-         maxcrr = (long int) (1000000.0 * size / sizeof(double)) ;
+	maxcrr = (long int) (1000000.0 * size / sizeof(double)) ;
       else if ((strcmp(type, "GB")==0) || (strcmp(type, "GBYTES")==0))
-         maxcrr = (long int) (1000000000.0 * size / sizeof(double)) ;
+	maxcrr = (long int) (1000000000.0 * size / sizeof(double)) ;
       else {
-         fprintf(outfile, "bad data type, specify one of: \n") ;
-         fprintf(outfile, "REAL, INTEGER, BYTES, KBYTES, MBYTES, or GBYTES\n") ;
-         fndcor_abort(infile, outfile) ;
-         }
+	fprintf(outfile, "bad data type, specify one of: \n") ;
+	fprintf(outfile, "REAL, INTEGER, BYTES, KBYTES, MBYTES, or GBYTES\n") ;
+	fndcor_abort(infile, outfile) ;
       }
+    }
+  }
   
-   *maxcrb = maxcrr * sizeof(double) ;
+  *maxcrb = maxcrr * sizeof(double) ;
 
-   return;
+  return;
 }
 
 
