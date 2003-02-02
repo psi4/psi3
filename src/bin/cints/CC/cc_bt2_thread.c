ee56452c623a0e69f85f1c76ac8b2016e47280a3
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-02-02 01:31:53 +0000

    Replaced loops with ones from te_deriv1_scf.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1462 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cints/CC/cc_bt2_thread.c
+++ src/bin/cints/CC/cc_bt2_thread.c
@@ -39,7 +39,8 @@ void *cc_bt2_thread(void *tnum_ptr)
   int n, num;
   int total_am, am;
   int orig_am[4];
-  int i, m, p, q, r, s;
+  int i, j, k, l, m, p, q, r, s;
+  int ni, nj, nk, nl;
   int p_abs, q_abs, r_abs, s_abs;
   int si, sj, sk, sl ;
   int sii, sjj, skk, sll , slll;
@@ -47,7 +48,7 @@ void *cc_bt2_thread(void *tnum_ptr)
   int pi, pj, pk, pl;
   int max_pj, max_pl;
   int *sj_arr, *sk_arr, *sl_arr;
-  int sr_fao, ss_fao, sp_fao, sq_fao;
+  int si_fao, sj_fao, sk_fao, sl_fao;
   int usii,usjj,uskk,usll,usi,usj,usk,usl,usij;
   int stab_i,stab_j,stab_k,stab_l,stab_ij,stab_kl;
   int *R_list, *S_list, *T_list;
@@ -55,9 +56,11 @@ void *cc_bt2_thread(void *tnum_ptr)
   int dcr_ij, dcr_kl, dcr_ijkl;
   int lambda_T = 1;
   int num_unique_quartets;
+  int quartet_index;
   int plquartet;
   int max_num_unique_quartets;
   int max_num_prim_comb;
+  int switch_ij, switch_kl, switch_ijkl;
 
   int size;
   int max_class_size;
@@ -75,7 +78,7 @@ void *cc_bt2_thread(void *tnum_ptr)
   int rs_offset, rsi_offset, rsp_offset;
 
   double AB2, CD2;
-
+  double value;
   double *raw_data;             /* pointer to the unnormalized taregt quartet of integrals */
   double *data;                 /* pointer to the transformed normalized target quartet of integrals */
 #ifdef NONDOUBLE_INTS
@@ -135,103 +138,60 @@ void *cc_bt2_thread(void *tnum_ptr)
 #endif
 
   
-/*-----------------------------------
-  generate all unique shell quartets
- -----------------------------------*/
-  /*--- "unique" R,S loop ---*/
-  usij = 0;
+  /*--------------------------------------------
+    generate all symmetry unique shell quartets
+   --------------------------------------------*/
   for (usii=0; usii<Symmetry.num_unique_shells; usii++)
-    for (usjj=0; usjj<=usii; usjj++, usij++) {
-      /*--- Decide if this thread will do this ---*/
-      if ( usij%UserOptions.num_threads != thread_num )
-	continue;
-      usi = usii; usj = usjj;
-      /*--- As usual, swap order usi and usj according to their angular momenta ---*/
-      if(BasisSet.shells[Symmetry.us2s[usi]].am < BasisSet.shells[Symmetry.us2s[usj]].am){
-	dum = usi;
-	usi = usj;
-	usj = dum;
-      }
-	
-      sii = Symmetry.us2s[usi];
-      sjj = Symmetry.us2s[usj];
-      if (Symmetry.nirreps > 1) {
-	stab_i = Symmetry.atom_positions[BasisSet.shells[sii].center-1];
-	stab_j = Symmetry.atom_positions[BasisSet.shells[sjj].center-1];
-	stab_ij = Symmetry.GnG[stab_i][stab_j];
-	R_list = Symmetry.dcr[stab_i][stab_j];
-	num_ij = Symmetry.dcr_dim[stab_i][stab_j];
-      }
-      else
-	num_ij = 1;
-      
-      /*--- R,S loop ---*/
-      for(dcr_ij=0;dcr_ij<num_ij;dcr_ij++) {
-	if (Symmetry.nirreps > 1)
-	  R = R_list[dcr_ij];
-	else
-	  R = 0;
-	si = sii;
-	sj = BasisSet.shells[sjj].trans_vec[R]-1;
-	
-	/*--- "Unique" R,S loop ---*/
-	for (uskk=0; uskk<Symmetry.num_unique_shells; uskk++)
-	  for (usll=0; usll<=uskk; usll++){
-	    
-	    /*--- For each combination of unique shells generate "petit list" of shells ---*/
-	    usk = uskk; usl = usll;
-	    /*--- As usual, swap order usk and usl according to their angular momenta ---*/
-	    if(BasisSet.shells[Symmetry.us2s[usk]].am < BasisSet.shells[Symmetry.us2s[usl]].am){
-	      dum = usk;
-	      usk = usl;
-	      usl = dum;
-	    }
-	    /*--- DO NOT SWAP bra and ket at this time. Do it later, in the main loop ---*/
-	    if(BasisSet.shells[Symmetry.us2s[usi]].am + BasisSet.shells[Symmetry.us2s[usj]].am >
-	       BasisSet.shells[Symmetry.us2s[usk]].am + BasisSet.shells[Symmetry.us2s[usl]].am)
-	      swap_ij_kl = 1;
-	    else
-	      swap_ij_kl = 0;
+    for (usjj=0; usjj<=usii; usjj++)
+      for (uskk=0; uskk<=usii; uskk++)
+	for (usll=0; usll<= ((usii == uskk) ? usjj : uskk); usll++, quartet_index++){
 
-	    skk = Symmetry.us2s[usk];
-	    sll = Symmetry.us2s[usl];
-	    if (Symmetry.nirreps > 1) { /*--- Non-C1 symmetry case ---*/
+	  usi = usii;
+	  usj = usjj;
+	  usk = uskk;
+	  usl = usll;
+
+	  /*--- Decide if this thread will do this ---*/
+	  if ( quartet_index%UserOptions.num_threads != thread_num )
+	    continue;
+
+	  si = Symmetry.us2s[usii];
+	  sjj = Symmetry.us2s[usjj];
+	  skk = Symmetry.us2s[uskk];
+	  sll = Symmetry.us2s[usll];
+
+	  if (Symmetry.nirreps > 1) { /*--- Non-C1 symmetry case ---*/
 	      /*--- Generate the petite list of shell quadruplets using DCD approach of Davidson ---*/
+	      stab_i = Symmetry.atom_positions[BasisSet.shells[si].center-1];
+	      stab_j = Symmetry.atom_positions[BasisSet.shells[sjj].center-1];
 	      stab_k = Symmetry.atom_positions[BasisSet.shells[skk].center-1];
 	      stab_l = Symmetry.atom_positions[BasisSet.shells[sll].center-1];
+	      stab_ij = Symmetry.GnG[stab_i][stab_j];
 	      stab_kl = Symmetry.GnG[stab_k][stab_l];
+	      R_list = Symmetry.dcr[stab_i][stab_j];
 	      S_list = Symmetry.dcr[stab_k][stab_l];
 	      T_list = Symmetry.dcr[stab_ij][stab_kl];
 	      lambda_T = Symmetry.nirreps/Symmetry.dcr_deg[stab_ij][stab_kl];
+	      ni = (BasisSet.puream ? 2*BasisSet.shells[si].am - 1 : ioff[BasisSet.shells[si].am]);
+	      nj = (BasisSet.puream ? 2*BasisSet.shells[sjj].am - 1 : ioff[BasisSet.shells[sjj].am]);
+	      nk = (BasisSet.puream ? 2*BasisSet.shells[skk].am - 1 : ioff[BasisSet.shells[skk].am]);
+	      nl = (BasisSet.puream ? 2*BasisSet.shells[sll].am - 1 : ioff[BasisSet.shells[sll].am]);
 
 	      memset(sj_arr,0,sizeof(int)*max_num_unique_quartets);
 	      memset(sk_arr,0,sizeof(int)*max_num_unique_quartets);
 	      memset(sl_arr,0,sizeof(int)*max_num_unique_quartets);
 	      count = 0;
-		
-	      for(dcr_ijkl=0;dcr_ijkl<Symmetry.dcr_dim[stab_ij][stab_kl];dcr_ijkl++){
-		T = T_list[dcr_ijkl];
-		sk = BasisSet.shells[skk].trans_vec[T]-1;
-		slll = BasisSet.shells[sll].trans_vec[T]-1;
-		for(dcr_kl=0;dcr_kl<Symmetry.dcr_dim[stab_k][stab_l];dcr_kl++) {
-		  S = S_list[dcr_kl];
-		  sl = BasisSet.shells[slll].trans_vec[S]-1;
-		  
-		  total_am = BasisSet.shells[si].am +
-		    BasisSet.shells[sj].am +
-		    BasisSet.shells[sk].am +
-		    BasisSet.shells[sl].am;
-		  /*-------------------------------------------------------------
-		    Obviously redundant or zero cases should be eliminated here!
-		    Right now only zero case is eliminated. Redundancies arising
-		    in DCD approach when usi == usj etc. may be eliminated too
-		    but lambda_T will have to be replaced by an array (it won't
-		    the same for every shell quartet in petite list anymore).
-		   -------------------------------------------------------------*/
-		  if(!(total_am%2)||
-		     (BasisSet.shells[si].center!=BasisSet.shells[sj].center)||
-		     (BasisSet.shells[sj].center!=BasisSet.shells[sk].center)||
-		     (BasisSet.shells[sk].center!=BasisSet.shells[sl].center)) {
+	      for(dcr_ij=0;dcr_ij<Symmetry.dcr_dim[stab_i][stab_j];dcr_ij++){
+		R = R_list[dcr_ij];
+		sj = BasisSet.shells[sjj].trans_vec[R]-1;
+		for(dcr_ijkl=0;dcr_ijkl<Symmetry.dcr_dim[stab_ij][stab_kl];dcr_ijkl++){
+		  T = T_list[dcr_ijkl];
+		  sk = BasisSet.shells[skk].trans_vec[T]-1;
+		  slll = BasisSet.shells[sll].trans_vec[T]-1;
+		  for(dcr_kl=0;dcr_kl<Symmetry.dcr_dim[stab_k][stab_l];dcr_kl++) {
+		    S = S_list[dcr_kl];
+		    sl = BasisSet.shells[slll].trans_vec[S]-1;
+
 		    sj_arr[count] = sj;
 		    sk_arr[count] = sk;
 		    sl_arr[count] = sl;
@@ -242,84 +202,114 @@ void *cc_bt2_thread(void *tnum_ptr)
 	      num_unique_quartets = count;
 	    }
 	    else { /*--- C1 symmetry case ---*/
-	      total_am = BasisSet.shells[si].am +
-		BasisSet.shells[usj].am +
-		BasisSet.shells[usk].am +
-		BasisSet.shells[usl].am;
-	      if(!(total_am%2)||
-		 (BasisSet.shells[si].center!=BasisSet.shells[usj].center)||
-		 (BasisSet.shells[usj].center!=BasisSet.shells[usk].center)||
-		 (BasisSet.shells[usk].center!=BasisSet.shells[usl].center)) {
-		num_unique_quartets = 1;
-		sj_arr[0] = usj;
-		sk_arr[0] = usk;
-		sl_arr[0] = usl;
-	      }
-	      else
-		num_unique_quartets = 0;
+	      num_unique_quartets = 1;
+	      sj_arr[0] = usj;
+	      sk_arr[0] = usk;
+	      sl_arr[0] = usl;
 	    }
+
+
+	  /*----------------------------------
+	    Compute the nonredundant quartets
+	   ----------------------------------*/
+	  for(plquartet=0;plquartet<num_unique_quartets;plquartet++) {
+	    si = Symmetry.us2s[usii];
+	    sj = sj_arr[plquartet];
+	    sk = sk_arr[plquartet];
+	    sl = sl_arr[plquartet];
+
+	    total_am = BasisSet.shells[si].am +
+	      BasisSet.shells[sj].am +
+	      BasisSet.shells[sk].am +
+	      BasisSet.shells[sl].am;
+	    /* parity selection */
+	    if (total_am%2 &&
+		BasisSet.shells[si].center!=BasisSet.shells[sj].center &&
+		BasisSet.shells[sj].center!=BasisSet.shells[sk].center &&
+		BasisSet.shells[sk].center!=BasisSet.shells[sl].center)
+	      continue;
+
+	    switch_ij = 0;
+	    switch_kl = 0;
+	    switch_ijkl = 0;
+	    /* place in "ascending" angular mom-
+	       my simple way of optimizing PHG recursion (VRR) */
+	    /* these first two are good for the HRR */
+	    if(BasisSet.shells[si].am < BasisSet.shells[sj].am){
+	      dum = si;
+	      si = sj;
+	      sj = dum;
+	      switch_ij = 1;
+	    }
+	    if(BasisSet.shells[sk].am < BasisSet.shells[sl].am){
+	      dum = sk;
+	      sk = sl;
+	      sl = dum;
+	      switch_kl = 1;
+	    }
+	    /* this should be /good/ for the VRR */
+	    if(BasisSet.shells[si].am + BasisSet.shells[sj].am > BasisSet.shells[sk].am + BasisSet.shells[sl].am){
+	      dum = si;
+	      si = sk;
+	      sk = dum;
+	      dum = sj;
+	      sj = sl;
+	      sl = dum;
+	      switch_ijkl = 1;
+	    }
+
+	    np_i = BasisSet.shells[si].n_prims;
+	    np_j = BasisSet.shells[sj].n_prims;
+	    np_k = BasisSet.shells[sk].n_prims;
+	    np_l = BasisSet.shells[sl].n_prims;
+	    ni = ioff[BasisSet.shells[si].am];
+	    nj = ioff[BasisSet.shells[sj].am];
+	    nk = ioff[BasisSet.shells[sk].am];
+	    nl = ioff[BasisSet.shells[sl].am];
+	    si_fao = BasisSet.shells[si].fao-1;
+	    sj_fao = BasisSet.shells[sj].fao-1;
+	    sk_fao = BasisSet.shells[sk].fao-1;
+	    sl_fao = BasisSet.shells[sl].fao-1;
+	    orig_am[0] = BasisSet.shells[si].am-1;
+	    orig_am[1] = BasisSet.shells[sj].am-1;
+	    orig_am[2] = BasisSet.shells[sk].am-1;
+	    orig_am[3] = BasisSet.shells[sl].am-1;
+	    am = orig_am[0] + orig_am[1] + orig_am[2] + orig_am[3];
 	    
-	    /*----------------------------------
-	      Compute the nonredundant quartets
-	     ----------------------------------*/
-	    for(plquartet=0;plquartet<num_unique_quartets;plquartet++) {
-	      si = sii;
-	      sj = sj_arr[plquartet];
-	      sk = sk_arr[plquartet];
-	      sl = sl_arr[plquartet];
-	      /*--- As usual, we have to order bra-ket so that ket has the largest angular momentum */
-	      if (swap_ij_kl) {
-		dum = si;
-		si = sk;
-		sk = dum;
-		dum = sj;
-		sj = sl;
-		sl = dum;
-	      }
-	      np_i = BasisSet.shells[si].n_prims;
-	      np_j = BasisSet.shells[sj].n_prims;
-	      np_k = BasisSet.shells[sk].n_prims;
-	      np_l = BasisSet.shells[sl].n_prims;
-	      orig_am[0] = BasisSet.shells[si].am-1;
-	      orig_am[1] = BasisSet.shells[sj].am-1;
-	      orig_am[2] = BasisSet.shells[sk].am-1;
-	      orig_am[3] = BasisSet.shells[sl].am-1;
-	      am = orig_am[0] + orig_am[1] + orig_am[2] + orig_am[3];
+	    sp_ij = &(BasisSet.shell_pairs[si][sj]);
+	    sp_kl = &(BasisSet.shell_pairs[sk][sl]);
+	    
+	    Libint.AB[0] = sp_ij->AB[0];
+	    Libint.AB[1] = sp_ij->AB[1];
+	    Libint.AB[2] = sp_ij->AB[2];
+	    Libint.CD[0] = sp_kl->AB[0];
+	    Libint.CD[1] = sp_kl->AB[1];
+	    Libint.CD[2] = sp_kl->AB[2];
 	      
-	      sp_ij = &(BasisSet.shell_pairs[si][sj]);
-	      sp_kl = &(BasisSet.shell_pairs[sk][sl]);
-	      
-	      Libint.AB[0] = sp_ij->AB[0];
-	      Libint.AB[1] = sp_ij->AB[1];
-	      Libint.AB[2] = sp_ij->AB[2];
-	      Libint.CD[0] = sp_kl->AB[0];
-	      Libint.CD[1] = sp_kl->AB[1];
-	      Libint.CD[2] = sp_kl->AB[2];
-	      
-	      AB2 = Libint.AB[0]*Libint.AB[0]+Libint.AB[1]*Libint.AB[1]+Libint.AB[2]*Libint.AB[2];
-	      CD2 = Libint.CD[0]*Libint.CD[0]+Libint.CD[1]*Libint.CD[1]+Libint.CD[2]*Libint.CD[2];
-	      
-	      /*--- Compute data for primitive quartets here ---*/
-	      num_prim_comb = 0;
-	      for (pi = 0; pi < np_i; pi++) {
-		max_pj = (si == sj) ? pi+1 : np_j;
-		for (pj = 0; pj < max_pj; pj++) {
-		  m = (1 + (si == sj && pi != pj));
-		  for (pk = 0; pk < np_k; pk++) {
-		    max_pl = (sk == sl) ? pk+1 : np_l;
-		    for (pl = 0; pl < max_pl; pl++){
-		      n = m * (1 + (sk == sl && pk != pl));
+	    AB2 = Libint.AB[0]*Libint.AB[0]+Libint.AB[1]*Libint.AB[1]+Libint.AB[2]*Libint.AB[2];
+	    CD2 = Libint.CD[0]*Libint.CD[0]+Libint.CD[1]*Libint.CD[1]+Libint.CD[2]*Libint.CD[2];
+	    
+	    /*--- Compute data for primitive quartets here ---*/
+	    num_prim_comb = 0;
+	    for (pi = 0; pi < np_i; pi++) {
+	      max_pj = (si == sj) ? pi+1 : np_j;
+	      for (pj = 0; pj < max_pj; pj++) {
+		m = (1 + (si == sj && pi != pj));
+		for (pk = 0; pk < np_k; pk++) {
+		  max_pl = (sk == sl) ? pk+1 : np_l;
+		  for (pl = 0; pl < max_pl; pl++){
+		    n = m * (1 + (sk == sl && pk != pl));
 #ifdef USE_TAYLOR_FM
-		      quartet_data(&(Libint.PrimQuartet[num_prim_comb++]), NULL, AB2, CD2,
-				   sp_ij, sp_kl, am, pi, pj, pk, pl, n*lambda_T);
+		    quartet_data(&(Libint.PrimQuartet[num_prim_comb++]), NULL, AB2, CD2,
+				 sp_ij, sp_kl, am, pi, pj, pk, pl, n*lambda_T);
 #else
-		      quartet_data(&(Libint.PrimQuartet[num_prim_comb++]), &fjt_table, AB2, CD2,
-				   sp_ij, sp_kl, am, pi, pj, pk, pl, n*lambda_T);
+		    quartet_data(&(Libint.PrimQuartet[num_prim_comb++]), &fjt_table, AB2, CD2,
+				 sp_ij, sp_kl, am, pi, pj, pk, pl, n*lambda_T);
 #endif
-		    }
 		  }
 		}
 	      }
+	    }
 
 	      /*--- Compute the integrals ---*/
 	      if (am) {
@@ -348,45 +338,19 @@ void *cc_bt2_thread(void *tnum_ptr)
 #endif
 	      }
 
-	      /*--- swap bra and ket back to the original order if needed ---*/
-	      if (swap_ij_kl) {
-		dum = si;
-		si = sk;
-		sk = dum;
-		dum = sj;
-		sj = sl;
-		sl = dum;
-		sr_fao = BasisSet.shells[si].fao - 1;
-		ss_fao = BasisSet.shells[sj].fao - 1;
-		sp_fao = BasisSet.shells[sk].fao - 1;
-		sq_fao = BasisSet.shells[sl].fao - 1;
-		nr = ioff[BasisSet.shells[si].am];
-		ns = ioff[BasisSet.shells[sj].am];
-		np = ioff[BasisSet.shells[sk].am];
-		nq = ioff[BasisSet.shells[sl].am];
-		if (am /*&& (orig_am[0] + orig_am[1] != 0)*/) {
-		  /*		    timer_on("Pre1Swap");*/
-		  /*--- (pq|rs) -> (rs|pq) ---*/
-		  ijkl_to_klij(data,scratch_buf,np*nq,nr*ns);
-		  data = scratch_buf;
-		  /*		    timer_off("Pre1Swap");*/
-		}
-	      }
-	      else {
-		sr_fao = BasisSet.shells[si].fao - 1;
-		ss_fao = BasisSet.shells[sj].fao - 1;
-		sp_fao = BasisSet.shells[sk].fao - 1;
-		sq_fao = BasisSet.shells[sl].fao - 1;
-		nr = ioff[BasisSet.shells[si].am];
-		ns = ioff[BasisSet.shells[sj].am];
-		np = ioff[BasisSet.shells[sk].am];
-		nq = ioff[BasisSet.shells[sl].am];
-	      }
 
-	    } /* end of RSPQ loop */
-	  } /* end of "unique" P,S loop */
-      } /* end of R,S loop */
-    } /* end of "unique" R,S loop */
+	      ijkl = 0;
+	      for(i=0;i<ni;i++)
+		for(j=0;j<nj;j++)
+		  for(k=0;k<nk;k++)
+		    for(l=0;l<nl;l++,ijkl++) {
+
+		      value = data[ijkl];
+
+		    }
+
+	  } /* end of RSPQ loop */
+	} /* end of "unique" RSQP loop */
 
   /*---------
     Clean-up
