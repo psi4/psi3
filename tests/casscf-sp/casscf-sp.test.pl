ff0e112c3f9c30b7ae7afeaaf519a14417049509
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-20 14:03:41 +0000

    Converted to the new test case format.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1611 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/casscf-sp/casscf-sp.test.pl
+++ tests/casscf-sp/casscf-sp.test.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl  
 
-$TOL = 10**-10;
+require ("../psitest.pl");
+
+$TOL = 10**-8;
 $REF_FILE = "file14.ref";
 $TEST_FILE = "file14.dat";
 $REF_OUT = "output.ref";
@@ -10,78 +12,30 @@ $RESULT = "casscf-sp.test";
 system ("input");
 system ("psi3");
 
-extract_data($REF_OUT,$REF_FILE,$Enuc_ref,$Ehf_ref,$Ecas_ref);
-extract_data($TEST_OUT,$TEST_FILE,$Enuc_test,$Ehf_test,$Ecas_test);
+open(RE, ">$RESULT") || die "cannot open $RESULT $!";
+select (RE);
+printf "\nCASSCF-SP:\n";
 
-compare_data();
-
-###############################################################################
-sub extract_data
-{
-  open(OUT, "$_[0]") || die "cannot open $_[0]: $!";
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/Nuclear Repulsion Energy    =/) {
-      @data1 = split(/ +/, $_);
-      $_[2] = $data1[4];
-    }
-  }
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/SCF total energy   =/) {
-      @data2 = split(/ +/, $_);
-      $_[3] = $data2[5];
-    }
-  }
-  
-  close (OUT);
-
-  open(OUT, "$_[1]") || die "cannot open $_[1]: $!";
-  
-  @cas = <OUT>;
-
-  @line1 = split(/ +/, $cas[0]);
-  $niter = $line1[1];
-  @line2 = split(/ +/, $cas[$niter]);
-  $_[4] = $line2[5];
-  close(OUT);
-
+if (abs(seek_nuc($REF_OUT) - seek_nuc($TEST_OUT)) > $TOL) {
+  fail_test("Nuclear Repulsion Energy");
 }
-###############################################################################
-sub compare_data
-{
-  open(RE, ">$RESULT") || die "cannot open $RESULT: $!";
-
-  select (RE);
-
-  printf "\nCASSCF-SP:\n";
-  
-  $diff_nuc = abs ($Enuc_ref - $Enuc_test);
-  if ($diff_nuc > $TOL) {
-    printf "\nNuclear Repulsion Energy ... FAILED\n";
-  }
-  else {
-    printf "\nNuclear Repulsion Energy ... PASSED\n";
-  }
-
-  $diff_hf = abs ($Ehf_ref - $Ehf_test);
-  if ($diff_hf > $TOL) {
-    printf "\nRHF Energy               ... FAILED\n";
-  }
-  else {
-    printf "\nRHF Energy               ... PASSED\n";
-  }
-
-  $diff_cas = abs ($Ecas_ref - $Ecas_test);
-  if ($diff_cas > $TOL) {
-    printf "\nCASSCF Energy            ... FAILED\n\n";
-  }
-  else {
-    printf "\nCASSCF Energy            ... PASSED\n\n";
-  }
-
-  close (RE);
+else {
+  pass_test("Nuclear Repulsion Energy");
 }
-###############################################################################
+
+if (abs(seek_scf($REF_OUT) - seek_scf($TEST_OUT)) > $TOL) {
+  fail_test("RHF Energy");
+}
+else {
+  pass_test("RHF Energy");
+}
+    
+if (abs(seek_casscf($REF_FILE) - seek_casscf($TEST_FILE)) > $TOL) {
+  fail_test("CASSCF Energy");
+}
+else {
+  pass_test("CASSCF Energy");
+}
+    
+close(RE);
+
