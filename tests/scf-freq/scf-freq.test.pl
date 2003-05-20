f86a88a220a7151762762a2b379106f8f0615fac
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-20 15:44:20 +0000

    Converted to the new test case format.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1619 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/scf-freq/scf-freq.test.pl
+++ tests/scf-freq/scf-freq.test.pl
@@ -1,121 +1,54 @@
 #!/usr/bin/perl  
 
-$TOL = 10**-10;
+require ("../psitest.pl");
+
+$TOL = 10**-8;
 $HTOL = 10**-2;
 $ITOL = 10**-3;
+$NDOF = 9;
 $REF_FILE = "output.ref";
 $TEST_FILE = "output.dat";
-$NDOF = 9;
 $RESULT = "scf-freq.test";
 
 system ("input");
 system ("psi3");
 
-extract_data($REF_FILE,$Enuc_ref,$Ehf_ref);
-extract_data($TEST_FILE,$Enuc_test,$Ehf_test);
+open(RE, ">$RESULT") || die "cannot open $RESULT: $!";
+select (RE);
+printf "\nSCF-FREQ:\n";
 
-compare_data();
-
-###############################################################################
-sub extract_data
-{
-  open(OUT, "$_[0]") || die "cannot open $_[0]: $!";
-  
-  @infile = <OUT>;
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/Nuclear Repulsion Energy    =/) {
-      @data1 = split(/ +/, $_);
-      $_[1] = $data1[4];
-    }
-  }
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/SCF total energy/) {
-      @data2 = split(/ +/, $_);
-      $_[2] = $data2[5];
-    }
-  }
-  
-  close(OUT);
-
-  $j=0;
-  $linenum=0;
-  foreach $line (@infile) {
-    $linenum++;
-    if ($line =~ m/Harmonic Frequency/) {
-      while ($j<$NDOF) {
-        @test = split (/ +/,$infile[$linenum+2+$j]);
-        if ($_[0] eq $REF_FILE) {
-          $hvf_ref[$j] = $test[2];
-          $int_ref[$j] = $test[3];
-        }
-        elsif ($_[0] eq $TEST_FILE) {
-          $hvf_test[$j] = $test[2];
-          $int_test[$j] = $test[3];
-        }
-        $j++;
-      }
-    }
-  }
- 
+if (abs(seek_nuc($REF_FILE) - seek_nuc($TEST_FILE)) > $TOL) {
+  fail_test("Nuclear Repulsion Energy");
 }
-###############################################################################
-sub compare_data
-{
-  open (RE, ">$RESULT") || die "cannot open $RESULT: $!";
-
-  select (RE);
-
-  printf "%20.12f %20.12f\n", $Enuc_ref, $Enuc_test;
-  printf "%20.12f %20.12f\n", $Ehf_ref, $Ehf_test;
-
-  printf "\nSCF-FREQ:\n";
-  $diff_nuc = abs ($Enuc_ref - $Enuc_test);
-  if ($diff_nuc > $TOL) {
-    printf "\nNuclear Repulsion Energy ... FAILED\n";
-  }
-  else {
-    printf "\nNuclear Repulsion Energy ... PASSED\n";
-  }
-
-  $diff_hf = abs ($Ehf_ref - $Ehf_test);
-  if ($diff_hf > $TOL) {
-    printf "\nRHF Energy               ... FAILED\n";
-  }
-  else {
-    printf "\nRHF Energy               ... PASSED\n";
-  }
-
-  for ($i=0; $i<$NDOF; $i++) {
-    $diff_hvf[$i] = abs ($hvf_ref[$i] - $hvf_test[$i]);
-    $diff_int[$i] = abs ($int_ref[$i] - $int_test[$i]);
-  }
-
-  for ($i=0; $i<$NDOF; $i++) {
-    if ($diff_hvf[$i] > $HTOL) {
-      $hout = "FAIL";
-    }
-    if ($diff_int[$i] > $ITOL) {
-      $iout = "FAIL";
-    }
-  }
-  
-  if ($hout eq "FAIL") {
-    printf "\nHarmonic Frequency       ... FAILED\n";
-  }
-  else {
-    printf "\nHarmonic Frequency       ... PASSED\n";
-  } 
-
-  if ($iout eq "FAIL") {
-    printf "\nInfrared Intensity       ... FAILED\n\n";
-  }
-  else {
-    printf "\nInfrared Intensity       ... PASSED\n\n";
-  }
-
+else {
+  pass_test("Nuclear Repulsion Energy");
 }
-###############################################################################
+
+if (abs(seek_scf($REF_FILE) - seek_scf($TEST_FILE)) > $TOL) {
+  fail_test("RHF Energy");
+}
+else {
+  pass_test("RHF Energy");
+}
+
+@freq_ref = seek_freq($REF_FILE,"Harmonic Frequency",$NDOF);
+@freq_test = seek_freq($TEST_FILE,"Harmonic Frequency",$NDOF);
+
+if(!compare_arrays(\@freq_ref, \@freq_test, $NDOF, 1, $HTOL)) {
+  fail_test("SCF Frequencies");
+}
+else {
+  pass_test("SCF Frequencies");
+}
+
+@int_ref = seek_int($REF_FILE,"Harmonic Frequency",$NDOF);
+@int_test = seek_int($TEST_FILE,"Harmonic Frequency",$NDOF);
+
+if(!compare_arrays(\@int_ref, \@int_test, $NDOF, 1, $ITOL)) {
+  fail_test("SCF Intensities");
+}
+else {
+  pass_test("SCF Intensities");
+}
+    
+close(RE);
