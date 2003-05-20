e825a9f3f0f11565513bac0bb60c46426fd630c6
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-20 19:42:01 +0000

    changed parameter in file11 parsing functions
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1633 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/cc2/cc2.test.pl
+++ tests/cc2/cc2.test.pl
@@ -11,21 +11,21 @@ $RESULT = "cc2.test";
 system ("input");
 system ("psi3");
 
-$natom = seek_natom_file11($REF_FILE,"CCSD");
+$natom = seek_natom_file11($REF_FILE,"iteration");
 
 open(RE, ">$RESULT") || die "cannot open $RESULT $!"; 
 select (RE);
 printf "\nCC2:\n";
 
-if(abs(seek_energy_file11($REF_FILE,"CCSD") - seek_energy_file11($TEST_FILE,"CCSD")) > $TOL) {
+if(abs(seek_energy_file11($REF_FILE,"iteration") - seek_energy_file11($TEST_FILE,"iteration")) > $TOL) {
   fail_test("CCSD energy");
 }
 else {
   pass_test("CCSD energy");
 }
 
-@geom_ref = seek_geom_file11($REF_FILE, "CCSD");
-@geom_test = seek_geom_file11($TEST_FILE, "CCSD");
+@geom_ref = seek_geom_file11($REF_FILE, "iteration");
+@geom_test = seek_geom_file11($TEST_FILE, "iteration");
 if(!compare_arrays(\@geom_ref, \@geom_test, $natom, 3, $GTOL)) {
   fail_test("CCSD Geometry");
 }
@@ -33,8 +33,8 @@ else {
   pass_test("CCSD Geometry");
 }
 
-@grad_ref = seek_grad_file11($REF_FILE, "CCSD");
-@grad_test = seek_grad_file11($TEST_FILE, "CCSD");
+@grad_ref = seek_grad_file11($REF_FILE, "iteration");
+@grad_test = seek_grad_file11($TEST_FILE, "iteration");
 
 if(!compare_arrays(\@grad_ref, \@grad_test, $natom, 3, $GTOL)) {
   fail_test("CCSD Gradient");
