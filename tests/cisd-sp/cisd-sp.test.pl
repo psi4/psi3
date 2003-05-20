e47997a182985b4cfa86585457cb5972b619b70f
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-20 14:13:57 +0000

    Converted to the new test case format.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1612 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/cisd-sp/cisd-sp.test.pl
+++ tests/cisd-sp/cisd-sp.test.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl  
 
-$TOL = 10**-10;
+require ("../psitest.pl");
+
+$TOL = 10**-8;
 $REF_FILE = "output.ref";
 $TEST_FILE = "output.dat";
 $RESULT = "cisd-sp.test";
@@ -8,74 +10,30 @@ $RESULT = "cisd-sp.test";
 system ("input");
 system ("psi3");
 
-extract_data($REF_FILE,$Enuc_ref,$Ehf_ref,$Eci_ref);
-extract_data($TEST_FILE,$Enuc_test,$Ehf_test,$Eci_test);
+open(RE, ">$RESULT") || die "cannot open $RESULT $!";
+select (RE);
+printf "\nCISD-SP:\n";
 
-compare_data();
-
-###############################################################################
-sub extract_data
-{
-  open(OUT, "$_[0]") || die "cannot open $_[0]: $!";
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/Nuclear Repulsion Energy    =/) {
-      @data1 = split(/ +/, $_);
-      $_[1] = $data1[4];
-    }
-  }
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/SCF total energy   =/) {
-      @data2 = split(/ +/, $_);
-      $_[2] = $data2[5];
-    }
-  }
-
-  seek(OUT,0,0);
-  while (<OUT>) {
-    if (/ROOT 1/) {
-      @data3 = split(/ +/, $_);
-      $_[3] = $data3[4];
-    }
-  }
-
-  close(OUT);
+if (abs(seek_nuc($REF_FILE) - seek_nuc($TEST_FILE)) > $TOL) {
+  fail_test("Nuclear Repulsion Energy");
 }
-###############################################################################
-sub compare_data
-{
-  open(RE, ">$RESULT") || die "cannot open $RESULT: $!";
-
-  select (RE);
-
-  printf "\nCISD-SP:\n";
-  
-  $diff_nuc = abs ($Enuc_ref - $Enuc_test);
-  if ($diff_nuc > $TOL) {
-    printf "\nNuclear Repulsion Energy ... FAILED\n";
-  }
-  else {
-    printf "\nNuclear Repulsion Energy ... PASSED\n";
-  }
-
-  $diff_hf = abs ($Ehf_ref - $Ehf_test);
-  if ($diff_hf > $TOL) {
-    printf "\nRHF Energy               ... FAILED\n";
-  }
-  else {
-    printf "\nRHF Energy               ... PASSED\n";
-  }
-
-  $diff_ci = abs ($Eci_ref - $Eci_test);
-  if ($diff_ci > $TOL) {
-    printf "\nCISD Energy              ... FAILED\n\n";
-  }
-  else {
-    printf "\nCISD Energy              ... PASSED\n\n";
-  }
-
-  close (RE);
+else {
+  pass_test("Nuclear Repulsion Energy");
 }
+
+if (abs(seek_scf($REF_FILE) - seek_scf($TEST_FILE)) > $TOL) {
+  fail_test("RHF Energy");
+}
+else {
+  pass_test("RHF Energy");
+}
+    
+if (abs(seek_ci($REF_FILE) - seek_ci($TEST_FILE)) > $TOL) {
+  fail_test("CISD Energy");
+}
+else {
+  pass_test("CISD Energy");
+}
+    
+close(RE);
+
