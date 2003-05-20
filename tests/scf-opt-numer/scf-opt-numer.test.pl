3dd2a9274bfb923057876b2c1ea6de6e0e8e6d7f
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-20 21:07:52 +0000

    Converted to new test case format
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1639 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/scf-opt-numer/scf-opt-numer.test.pl
+++ tests/scf-opt-numer/scf-opt-numer.test.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl  
 
-$TOL = 10**-10;
+require("../psitest.pl");
+
+$TOL = 10**-8;
 $GTOL = 10**-8;
 $REF_FILE = "file11.ref";
 $TEST_FILE = "psi.file11.dat";
@@ -9,97 +11,36 @@ $RESULT = "scf-opt-numer.test";
 system ("input");
 system ("psi3");
 
-extract_data($REF_FILE,$Escf_ref);
-extract_data($TEST_FILE,$Escf_test);
+$natom = seek_natom_file11($REF_FILE,"iteration");
 
-compare_data();
+open(RE, ">$RESULT") || die "cannot open $RESULT $!"; 
+select (RE);
+printf "\nSCF-OPT-NUMER:\n";
 
-###############################################################################
-sub extract_data
-{
-  # Open input file, place into array for parsing, close input file
-  open(IN, "$_[0]") || die "cannot open $_[0] $!";
-  @infile = <IN>; #file11 is copied to @outfile
-  close (IN);
-
-  # Initialize variables
-  $match = "iteration";
-  $linenum = 0;
-  $lastiter = 0;
-  $flag = 0;
-
-  foreach $line (@infile) {
-    $linenum++;
-    if ($line =~ m/$match/) {
-      if ($flag == 0) {
-        $flag = 1;
-      }
-      $lastiter = $linenum;
-    }
-  }
-
-  @line = split (/ +/, $infile[$lastiter]);
-  $natom = $line[1];
-  $_[1] = $line[2];
-
-  $j=1;
-  while ($j<$natom+1) {
-    @line2 = split (/ +/, $infile[$lastiter+$j]);
-    if ($_[0] eq $REF_FILE) {
-      $x_ref[$j-1] = $line2[2];
-      $y_ref[$j-1] = $line2[3];
-      $z_ref[$j-1] = $line2[4];
-    }
-    elsif ($_[0] eq $TEST_FILE) {
-      $x_test[$j-1] = $line2[2];
-      $y_test[$j-1] = $line2[3];
-      $z_test[$j-1] = $line2[4];
-    }
-    $j++;
-  }
+if(abs(seek_energy_file11($REF_FILE,"iteration") - seek_energy_file11($TEST_FILE,"iteration")) > $TOL) {
+  fail_test("SCF energy");
 }
-###############################################################################
-sub compare_data
-{
-  open (RE, ">$RESULT") || die "cannot open $RESULT $!";
-  select (RE);
-  
-  printf "\nSCF-OPT-NUMER:\n";
-  
-  $diff_scf = abs ($Escf_ref - $Escf_test);
-
-  if ($diff_scf > $TOL) {
-    printf "\nSCF Energy           ... FAILED\n";
-  }
-  else {
-    printf "\nSCF Energy           ... PASSED\n";
-  }
-
-  for ($i=0; $i<$natom; $i++) {
-    $diff_x[$i] = abs ($x_ref[$i] - $x_test[$i]);
-    $diff_y[$i] = abs ($y_ref[$i] - $y_test[$i]);
-    $diff_z[$i] = abs ($z_ref[$i] - $z_test[$i]);
-  }
-
-  for ($i=0; $i<$natom; $i++) {
-    if ($diff_x[$i] > $GTOL) {
-      $out = "FAIL";
-    }
-    if ($diff_y[$i] > $GTOL) {
-      $out = "FAIL";
-    }
-    if ($diff_z[$i] > $GTOL) {
-      $out = "FAIL";
-    }
-  }
-
-  if ($out eq "FAIL") {
-    printf "\nGeometry             ... FAILED\n\n";
-  }
-  else {
-    printf "\nGeometry             ... PASSED\n\n";
-  }
-
-  close (RE);
+else {
+  pass_test("SCF energy");
 }
-###############################################################################
+
+@geom_ref = seek_geom_file11($REF_FILE, "iteration");
+@geom_test = seek_geom_file11($TEST_FILE, "iteration");
+if(!compare_arrays(\@geom_ref, \@geom_test, $natom, 3, $GTOL)) {
+  fail_test("SCF Geometry");
+}
+else {
+  pass_test("SCF Geometry");
+}
+
+@grad_ref = seek_grad_file11($REF_FILE, "iteration");
+@grad_test = seek_grad_file11($TEST_FILE, "iteration");
+
+if(!compare_arrays(\@grad_ref, \@grad_test, $natom, 3, $GTOL)) {
+  fail_test("SCF Gradient");
+}
+else {
+  pass_test("SCF Gradient");
+}
+
+close (RE);
