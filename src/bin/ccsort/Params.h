8e38734418bcb775ebd15ff536c40b358bde0920
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-05-30 19:39:49 +0000

    Added parameter to determine if (ab|cd) need to be sorted.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1661 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccsort/Params.h
+++ src/bin/ccsort/Params.h
@@ -3,12 +3,13 @@ struct Params {
   int keep_TEIFile;      /* Should we keep the input two-elec. integrals? */
   int keep_OEIFile;      /* Should we keep the input one-elec. integrals? */
   double tolerance;      /* Cutoff value for integrals in IWL Buffers */
-  long int memory;            /* Memory available (in bytes) */
+  long int memory;       /* Memory available (in bytes) */
   int cachelev;
   int ref;
   char *wfn;
+  int make_abcd;
   int dertype;
   char *aobasis;
-  int reset;  /* cmdline argument; if true, all CC-related files are deleted 
-		 at the beginning of the run */
+  int reset;            /* cmdline argument; if true, all CC-related files */
+                        /* are deleted at the beginning of the run */
 };
