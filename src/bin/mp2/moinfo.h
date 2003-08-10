e1543ec3c9217361ddbeb3a04b30c8ceb5c21279
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2003-08-10 17:54:55 +0000

    Preparing for official PSI3.2alpha release.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@1787 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/mp2/moinfo.h
+++ src/bin/mp2/moinfo.h
@@ -4,7 +4,7 @@ struct moinfo {
   int nirreps;           /* no. of irreducible representations */
   int nfzdocc;           /* no. of frozen occupied MOs */
   int nfzvirt;           /* no. of frozen virtual MOs */
-  int nactmo;
+  int nactmo;            /* no. of active MOs */
   int nactdocc;          /* no. of active occupied MOs */
   int nactvirt;          /* no. of active virtual MOs */
   int ndocc;             /* no. of all occupied MOs */  
@@ -12,9 +12,9 @@ struct moinfo {
   int *fzdoccpi;         /* frozen occupied MOs per irrep */
   int *fzvirtpi;         /* frozen virtual MOs per irrep */
   int *actdoccpi;        /* active occupied MOs per irrep */
-  int *actdoccsym;
+  int *actdoccsym;       /* active occupied MOs symmetry */
   int *actvirtpi;        /* active virtual MOs per irrep */
-  int *actvirtsym;
+  int *actvirtsym;       /* active virtual MOs symmetry */
   int *doccpi;           /* all occupied MOs per irrep */
   int *virtpi;           /* all virtual MOs per irrep */
   int *mopi;             /* all MOs per irrep */
@@ -22,10 +22,9 @@ struct moinfo {
   double Enuc;           /* Nuclear repulsion energy */
   double Escf;           /* SCF energy */
   double *scfevals;      /* SCF eigenvalues */
-  char **irreplabels;
-  int *docc_off;
-  int *virt_off;
-  int *qt_docc;
-  int *qt_virt;
-  double **opdm;
+  char **irreplabels;    /* irrep labels */
+  int *docc_off;         /* occupied orbital offsets within each irrep */ 
+  int *virt_off;         /* virtual orbital offsets within each irrep */
+  int *qt_docc;          /* CC->QT active occupied reordering array */
+  int *qt_virt;          /* CC->QT active virtual reordering array */
 };
