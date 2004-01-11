f6d14a7c153ebff2b8571b88bcb05b157cdc320d
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2004-01-11 21:13:39 +0000

    Added a whole bunch of new types of integrals + documented format for previously added property integrals.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2222 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- include/psifiles.h
+++ include/psifiles.h
@@ -62,7 +62,8 @@
 #define PSIF_BB_PRESORT     88   /* BB UHF twopdm presort file */
 #define PSIF_AB_PRESORT     89   /* AB UHF twopdm presort file */
 
-/* All of these one-electron quantities have been moved into PSIF_OEI */
+/* All of these one-electron quantities have been moved into PSIF_OEI
+   Most integrals are real Hermitian hence only lower triangle of the matrix is written out */
 /* These macros give libpsio TOC strings for easy identification.     */
 #define PSIF_SO_S           "SO-basis Overlap Ints"
 #define PSIF_SO_T           "SO-basis Kinetic Energy Ints"
@@ -74,12 +75,38 @@
 #define PSIF_MO_MX          "MO-basis Mu-X Ints"
 #define PSIF_MO_MY          "MO-basis Mu-Y Ints"
 #define PSIF_MO_MZ          "MO-basis Mu-Z Ints"
-#define PSIF_AO_NablaX      "AO-basis Nabla-X Ints"
+#define PSIF_AO_QXX         "AO-basis Q-XX Ints"    /* Electric quadrupole moment integrals */
+#define PSIF_AO_QXY         "AO-basis Q-XY Ints"
+#define PSIF_AO_QXZ         "AO-basis Q-XZ Ints"
+#define PSIF_AO_QYY         "AO-basis Q-YY Ints"
+#define PSIF_AO_QYZ         "AO-basis Q-YZ Ints"
+#define PSIF_AO_QZZ         "AO-basis Q-ZZ Ints"
+
+/* These integrals are anti-Hermitian -- upper triangle has sign opposite of that of the lower triangle */
+#define PSIF_AO_NablaX      "AO-basis Nabla-X Ints" /* integrals of nabla operator */
 #define PSIF_AO_NablaY      "AO-basis Nabla-Y Ints"
 #define PSIF_AO_NablaZ      "AO-basis Nabla-Z Ints"
-#define PSIF_AO_LX          "AO-basis LX Ints"
+
+/* These integrals are pure imaginary Hermitian. We write the full matrix out */
+#define PSIF_AO_LX          "AO-basis LX Ints"      /* integrals of angular momentum operator */
 #define PSIF_AO_LY          "AO-basis LY Ints"
 #define PSIF_AO_LZ          "AO-basis LZ Ints"
+#define PSIF_AO_DSDB_X      "AO-basis dS/dBx Ints"      /* Overlap derivative integrals WRT B field */
+#define PSIF_AO_DSDB_Y      "AO-basis dS/dBy Ints"
+#define PSIF_AO_DSDB_Z      "AO-basis dS/dBz Ints"
+#define PSIF_AO_DHDB_X      "AO-basis dh/dBx Ints"      /* One-electron derivative integrals WRT B field */
+#define PSIF_AO_DHDB_Y      "AO-basis dh/dBy Ints"
+#define PSIF_AO_DHDB_Z      "AO-basis dh/dBz Ints"
+#define PSIF_AO_D2HDBDE_XX  "AO-basis d2h/dBxdEx Ints"  /* One-electron derivative integrals WRT E and B fields */
+#define PSIF_AO_D2HDBDE_XY  "AO-basis d2h/dBxdEy Ints"
+#define PSIF_AO_D2HDBDE_XZ  "AO-basis d2h/dBxdEz Ints"
+#define PSIF_AO_D2HDBDE_YX  "AO-basis d2h/dBydEx Ints"
+#define PSIF_AO_D2HDBDE_YY  "AO-basis d2h/dBydEy Ints"
+#define PSIF_AO_D2HDBDE_YZ  "AO-basis d2h/dBydEz Ints"
+#define PSIF_AO_D2HDBDE_ZX  "AO-basis d2h/dBzdEx Ints"
+#define PSIF_AO_D2HDBDE_ZY  "AO-basis d2h/dBzdEy Ints"
+#define PSIF_AO_D2HDBDE_ZZ  "AO-basis d2h/dBzdEz Ints"
+
 #define PSIF_MO_OEI         "MO-basis One-electron Ints"
 #define PSIF_MO_A_OEI       "MO-basis Alpha One-electron Ints"
 #define PSIF_MO_B_OEI       "MO-basis Beta One-electron Ints"
