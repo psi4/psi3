489486cd75aa3439929e95da8e700435b0cfbca3
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2004-05-03 04:48:08 +0000

    Adding CPHF code to ccsort for later local response calcs. -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2456 29f243b5-bcef-41bb-bbe7-8e20262f3206

---
+++ src/bin/ccsort/cphf_F.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libqt/qt.h>
+#include <libdpd/dpd.h>
+#include <libciomr/libciomr.h>
+#include <psifiles.h>
+#include "MOInfo.h"
+#define EXTERN
+#include "globals.h"
+
+void cphf_F(void)
+{
+  int irrep, row, a, i, asym, isym, num_ai, info, *ipiv;
+  double *vector, *vector2, polar;
+  dpdbuf4 A;
+  dpdfile2 mu;
+
+  psio_open(PSIF_MO_HESS, 1);
+  dpd_buf4_init(&A, PSIF_MO_HESS, 0, 11, 11, 11, 11, 0, "A(AI,BJ)");
+
+  irrep = moinfo.irrep_x;
+
+  /* sort Mu elements into a single vector for lineq solver */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "Mu_X_AI");
+  dpd_file2_mat_init(&mu);
+  dpd_file2_mat_rd(&mu);
+  num_ai = A.params->rowtot[irrep];
+  vector = init_array(num_ai);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    vector[row] = mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]];
+  }
+  dpd_file2_mat_close(&mu);
+  dpd_file2_close(&mu);
+
+  /* grab current irrep of MO Hessian */
+  dpd_buf4_mat_irrep_init(&A, irrep);
+  dpd_buf4_mat_irrep_rd(&A, irrep);
+
+
+  /* solve CPHF equations */
+  ipiv = init_int_array(num_ai);
+  info = C_DGESV(num_ai, 1, &(A.matrix[irrep][0][0]), num_ai, ipiv, vector, num_ai);
+  if(info) {
+    fprintf(outfile, "CCSORT: cphf_F: Error in C_DGESV.  Info = %d.  Exiting.\n", info);
+    exit(PSI_RETURN_FAILURE);
+  }
+  free(ipiv);
+
+  dpd_buf4_mat_irrep_close(&A, irrep);
+
+  /* sort CPHF solution to DPD format */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "CPHF U_X_AI");
+  dpd_file2_mat_init(&mu);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]] = vector[row];
+  }
+  dpd_file2_mat_wrt(&mu);
+  dpd_file2_close(&mu);
+
+  irrep = moinfo.irrep_y;
+
+  /* sort Mu elements into a single vector for lineq solver */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "Mu_Y_AI");
+  dpd_file2_mat_init(&mu);
+  dpd_file2_mat_rd(&mu);
+  num_ai = A.params->rowtot[irrep];
+  vector = init_array(num_ai);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    vector[row] = mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]];
+  }
+  dpd_file2_mat_close(&mu);
+  dpd_file2_close(&mu);
+
+  /* grab current irrep of MO Hessian */
+  dpd_buf4_mat_irrep_init(&A, irrep);
+  dpd_buf4_mat_irrep_rd(&A, irrep);
+
+
+  /* solve CPHF equations */
+  ipiv = init_int_array(num_ai);
+  info = C_DGESV(num_ai, 1, &(A.matrix[irrep][0][0]), num_ai, ipiv, vector, num_ai);
+  if(info) {
+    fprintf(outfile, "CCSORT: cphf_F: Error in C_DGESV.  Info = %d.  Exiting.\n", info);
+    exit(PSI_RETURN_FAILURE);
+  }
+  free(ipiv);
+
+  dpd_buf4_mat_irrep_close(&A, irrep);
+
+  /* sort CPHF solution to DPD format */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "CPHF U_Y_AI");
+  dpd_file2_mat_init(&mu);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]] = vector[row];
+  }
+  dpd_file2_mat_wrt(&mu);
+  dpd_file2_close(&mu);
+
+  irrep = moinfo.irrep_z;
+
+  /* sort Mu elements into a single vector for lineq solver */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "Mu_Z_AI");
+  dpd_file2_mat_init(&mu);
+  dpd_file2_mat_rd(&mu);
+  num_ai = A.params->rowtot[irrep];
+  vector = init_array(num_ai);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    vector[row] = mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]];
+  }
+  dpd_file2_mat_close(&mu);
+  dpd_file2_close(&mu);
+
+  /*
+  vector2 = init_array(num_ai);
+  for(row=0; row < num_ai; row++) vector2[row] = vector[row];
+  */
+
+  /* grab current irrep of MO Hessian */
+  dpd_buf4_mat_irrep_init(&A, irrep);
+  dpd_buf4_mat_irrep_rd(&A, irrep);
+
+  /* solve CPHF equations */
+  ipiv = init_int_array(num_ai);
+  info = C_DGESV(num_ai, 1, &(A.matrix[irrep][0][0]), num_ai, ipiv, vector, num_ai);
+  if(info) {
+    fprintf(outfile, "CCSORT: cphf_F: Error in C_DGESV.  Info = %d.  Exiting.\n", info);
+    exit(PSI_RETURN_FAILURE);
+  }
+  free(ipiv);
+
+  dpd_buf4_mat_irrep_close(&A, irrep);
+
+  /*
+  polar = 0.0;
+  for(row=0; row < num_ai; row++) polar += vector2[row] * vector[row];
+  polar *= 4.0;
+  fprintf(outfile, "\talpha_zz = %20.12f\n", polar);
+  free(vector2);
+  */
+
+  /* sort CPHF solution to DPD format */
+  dpd_file2_init(&mu, CC_OEI, irrep, 1, 0, "CPHF U_Z_AI");
+  dpd_file2_mat_init(&mu);
+  for(row=0; row < num_ai; row++) {
+    a = A.params->roworb[irrep][row][0];
+    i = A.params->roworb[irrep][row][1];
+    asym = A.params->psym[a];
+    isym = A.params->qsym[i];
+    mu.matrix[asym][a-A.params->poff[asym]][i-A.params->qoff[isym]] = vector[row];
+  }
+  dpd_file2_mat_wrt(&mu);
+  dpd_file2_close(&mu);
+
+  dpd_buf4_close(&A);
+  psio_close(PSIF_MO_HESS, 1);
+}
---
+++ src/bin/ccsort/transmu.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <libciomr/libciomr.h>
+#include <libqt/qt.h>
+#include <libiwl/iwl.h>
+#include <psifiles.h>
+#include "MOInfo.h"
+#define EXTERN
+#include "globals.h"
+
+void transmu(void)
+{
+  int stat, nao, noei_ao, nso, nmo;
+  int i, j, ij;
+  double *scratch, **TMP, **X;
+  double **MUX, **MUY, **MUZ;  /* MO-basis dipole integrals */
+
+  nao = moinfo.nao;
+  nso = moinfo.nso;
+  nmo = moinfo.nmo;
+  noei_ao = moinfo.nao*(moinfo.nao+1)/2;
+
+  /**** Transform the dipole integrals to the MO basis ****/
+
+  MUX = block_matrix(nmo,nmo);
+  MUY = block_matrix(nmo,nmo);
+  MUZ = block_matrix(nmo,nmo);
+
+  TMP = block_matrix(nao, nao);
+  X = block_matrix(nao, nao);
+  scratch = init_array(noei_ao);
+
+  stat = iwl_rdone(PSIF_OEI, PSIF_AO_MX, scratch, noei_ao, 0, 0, outfile);
+  for(i=0,ij=0; i < nao; i++)
+    for(j=0; j <= i; j++, ij++) {
+      TMP[i][j] = TMP[j][i] = scratch[ij];
+    }
+
+  C_DGEMM('n','t',nao,nso,nao,1,&(TMP[0][0]),nao,&(moinfo.usotao[0][0]),nao,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('n','n',nso,nso,nao,1,&(moinfo.usotao[0][0]),nao,&(X[0][0]),nao,
+	  0,&(TMP[0][0]),nao);
+
+  C_DGEMM('n','n',nso,nmo,nso,1,&(TMP[0][0]),nao,&(moinfo.scf[0][0]),nmo,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('t','n',nmo,nmo,nso,1,&(moinfo.scf[0][0]),nmo,&(X[0][0]),nao,
+	  0,&(MUX[0][0]),nmo);
+
+  zero_arr(scratch,noei_ao);
+
+  stat = iwl_rdone(PSIF_OEI, PSIF_AO_MY, scratch, noei_ao, 0, 0, outfile);
+  for(i=0,ij=0; i < nao; i++)
+    for(j=0; j <= i; j++, ij++) {
+      TMP[i][j] = TMP[j][i] = scratch[ij];
+    }
+
+  C_DGEMM('n','t',nao,nso,nao,1,&(TMP[0][0]),nao,&(moinfo.usotao[0][0]),nao,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('n','n',nso,nso,nao,1,&(moinfo.usotao[0][0]),nao,&(X[0][0]),nao,
+	  0,&(TMP[0][0]),nao);
+
+  C_DGEMM('n','n',nso,nmo,nso,1,&(TMP[0][0]),nao,&(moinfo.scf[0][0]),nmo,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('t','n',nmo,nmo,nso,1,&(moinfo.scf[0][0]),nmo,&(X[0][0]),nao,
+	  0,&(MUY[0][0]),nmo);
+
+  zero_arr(scratch,noei_ao);
+
+  stat = iwl_rdone(PSIF_OEI, PSIF_AO_MZ, scratch, noei_ao, 0, 0, outfile);
+  for(i=0,ij=0; i < nao; i++)
+    for(j=0; j <= i; j++, ij++) {
+      TMP[i][j] = TMP[j][i] = scratch[ij];
+    }
+
+  C_DGEMM('n','t',nao,nso,nao,1,&(TMP[0][0]),nao,&(moinfo.usotao[0][0]),nao,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('n','n',nso,nso,nao,1,&(moinfo.usotao[0][0]),nao,&(X[0][0]),nao,
+	  0,&(TMP[0][0]),nao);
+
+  C_DGEMM('n','n',nso,nmo,nso,1,&(TMP[0][0]),nao,&(moinfo.scf[0][0]),nmo,
+	  0,&(X[0][0]),nao);
+  C_DGEMM('t','n',nmo,nmo,nso,1,&(moinfo.scf[0][0]),nmo,&(X[0][0]),nao,
+	  0,&(MUZ[0][0]),nmo);
+
+  free_block(TMP);
+  free_block(X);
+  free(scratch);
+
+  /*
+  fprintf(outfile, "MO-Basis MuX Integrals:\n");
+  mat_print(MUX, nmo, nmo, outfile);
+  fprintf(outfile, "MO-Basis MuY Integrals:\n");
+  mat_print(MUY, nmo, nmo, outfile);
+  fprintf(outfile, "MO-Basis MuZ Integrals:\n");
+  print_mat(MUZ, nmo, nmo, outfile);
+  */
+
+  moinfo.MUX = MUX;
+  moinfo.MUY = MUY;
+  moinfo.MUZ = MUZ;
+}
