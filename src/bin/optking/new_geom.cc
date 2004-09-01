77bede31636f5141fefb8d7695267c333c5b4616
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2004-09-01 22:02:29 +0000

    Geometry symmetrization did not work properly (need to use standard projector formula).
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2577 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/optking/new_geom.cc
+++ src/bin/optking/new_geom.cc
@@ -263,6 +263,7 @@ void symmetrize_geom(double *x) {
   int ua, op, xyz, num_uniques, i;
   int atom1, atom2, *ua2a, **ict, diag_ind, natom;
   double **cartrep, *x_temp;
+  int stab_order;
 
   chkpt_init(PSIO_OPEN_OLD);
   num_uniques = chkpt_rd_num_unique_atom();
@@ -277,11 +278,17 @@ void symmetrize_geom(double *x) {
 
   for(ua=0; ua<num_uniques; ua++) {
     atom1 = ua2a[ua];
+    stab_order = 0;
+    for(op=0; op < nirreps; op++) {
+      atom2 = ict[op][atom1] - 1;
+      if (atom1 == atom2)
+        stab_order++;
+    }
     for(op=0; op < nirreps; op++) {
       atom2 = ict[op][atom1] - 1;
       for(xyz=0; xyz<3; xyz++) {
         diag_ind = xyz*3 + xyz;
-        x_temp[3*atom2+xyz] = cartrep[op][diag_ind] * x[3*atom1+xyz];
+        x_temp[3*atom2+xyz] += cartrep[op][diag_ind] * x[3*atom1+xyz] / stab_order;
       }
     }
   }
