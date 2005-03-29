223f304d21ef6a4602fa1be3403f5f6a29255b9e
nruss <nruss@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-03-29 20:58:58 +0000

    Removed storage requirements for cc2_hbar_extra. -NJR
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2752 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cclambda/cc2_hbar_extra.c
+++ src/bin/cclambda/cc2_hbar_extra.c
@@ -8,18 +8,14 @@ void cc2_hbar_extra(void) {
 
   if(params.ref == 0) { /** RHF **/
     /* 2 W(ME,jb) + W(Me,Jb) */
-    dpd_buf4_init(&W1, CC_HBAR, 0, 10, 10, 10, 10, 0, "CC2 WMbeJ");
-    dpd_buf4_copy(&W1, CC_HBAR, "CC2 2 W(ME,jb) + W(Me,Jb)");
+    dpd_buf4_init(&W1, CC2_HET1, 0, 10, 10, 10, 10, 0, "CC2 WMbeJ (Me,Jb)");
+    dpd_buf4_copy(&W1, CC2_HET1, "CC2 2 W(ME,jb) + W(Me,Jb)");
     dpd_buf4_close(&W1);
-    dpd_buf4_init(&W1, CC_HBAR, 0, 10, 10, 10, 10, 0, "CC2 2 W(ME,jb) + W(Me,Jb)");
-    dpd_buf4_init(&W2, CC_HBAR, 0, 10, 10, 10, 10, 0, "CC2 WMbEj");
+    dpd_buf4_init(&W1, CC2_HET1, 0, 10, 10, 10, 10, 0, "CC2 2 W(ME,jb) + W(Me,Jb)");
+    dpd_buf4_init(&W2, CC2_HET1, 0, 10, 10, 10, 10, 0, "CC2 WMbEj (ME,jb)");
     dpd_buf4_axpy(&W2, &W1, 2);
     dpd_buf4_close(&W2);
     dpd_buf4_close(&W1);
 
-    dpd_buf4_init(&W, CC_HBAR, 0, 11, 5, 11, 5, 0, "WAmEf");
-    dpd_buf4_scmcopy(&W, CC_HBAR, "CC2 WAmEf 2(Am,Ef) - (Am,fE)", 2);
-    dpd_buf4_sort_axpy(&W, CC_HBAR, pqsr, 11, 5, "CC2 WAmEf 2(Am,Ef) - (Am,fE)", -1);
-    dpd_buf4_close(&W);
   }
 }
