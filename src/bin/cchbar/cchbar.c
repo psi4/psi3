49b70b51a10e36c121aa2d476dcc65dafe3910a8
nruss <nruss@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-03-29 20:54:53 +0000

    Fixed exit_io function. -NJR
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2750 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cchbar/cchbar.c
+++ src/bin/cchbar/cchbar.c
@@ -166,7 +166,7 @@ void exit_io(void)
   /* Close all dpd data files here */
   for(i=CC_MIN; i < CC_TMP; i++) psio_close(i,1);
   for(i=CC_TMP; i <= CC_TMP11; i++) psio_close(i,0);  /* get rid of TMP files */
-  for(i=CC_TMP11+1; i < CC_MAX; i++) psio_close(i,1);
+  for(i=CC_TMP11+1; i <= CC_MAX; i++) psio_close(i,1);
 
   psio_done();
   tstop(outfile);
