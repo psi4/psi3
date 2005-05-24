16b2fc9eb875566c288f3123be03ede9c79523ed
nruss <nruss@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-05-24 14:55:46 +0000

    Delected unused amplitude sorts. -NJR
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2790 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccresponse/cc2_sort_X.c
+++ src/bin/ccresponse/cc2_sort_X.c
@@ -11,8 +11,6 @@ void cc2_sort_X(char *pert, char *cart, int irrep, double omega)
 
   sprintf(lbl, "X_%s_%1s_IjAb (%5.3f)", pert, cart, omega);
   dpd_buf4_init(&X, CC_LR, irrep, 0, 5, 0, 5, 0, lbl);
-  sprintf(lbl, "X_%s_%1s_IbjA (%5.3f)", pert, cart, omega);
-  dpd_buf4_sort(&X, CC_LR, psqr, 10, 10, lbl);
   sprintf(lbl, "X_%s_%1s_(2IjAb-IjbA) (%5.3f)", pert, cart, omega);
   dpd_buf4_scmcopy(&X, CC_LR, lbl, 2);
   dpd_buf4_sort_axpy(&X, CC_LR, pqsr, 0, 5, lbl, -1);
