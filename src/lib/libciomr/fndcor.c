1d35f8ab84127cb6a281153b9b0db7df6f847b53
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-06-06 19:29:33 +0000

    Added stdlib.h for malloc.  -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2806 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libciomr/fndcor.c
+++ src/lib/libciomr/fndcor.c
@@ -4,6 +4,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <libipv1/ip_lib.h>
 #include <psifiles.h>
 
@@ -107,4 +108,3 @@ static void fndcor_abort(FILE *infile, FILE *outfile)
    exit(PSI_RETURN_FAILURE);
 }
  
-
