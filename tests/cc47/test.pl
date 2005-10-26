689795ded516d4995323aa84745d378036cb20a5
mabrams <mabrams@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-10-26 19:29:23 +0000

    Corrected test case. -MLA
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2944 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- tests/cc47/test.pl
+++ tests/cc47/test.pl
@@ -9,17 +9,14 @@ $TTOL = 10**-4;
 
 $REF_FILE = "output.ref";
 $TEST_FILE = "output.dat";
-$RESULT = "output.test";
 
 system ($PSICMD);
 
 $FAIL = 0;
 
-open(RE, ">$RESULT") || die "cannot open $RESULT: $!";
-select (RE);
-printf "EOM CCSD OEPROP:\n";
+print "EOM CCSD OEPROP:\n";
 
-$NSTATES = seek_nstates($REF_FILE);
+select(STDOUT);
 
 $LABEL = "Nuclear Repulsion Energy";
 if (abs(seek_nuc($REF_FILE) - seek_nuc($TEST_FILE)) > $TOL) {
@@ -39,11 +36,25 @@ else {
   printf "%-70s...PASSED\n",$LABEL;
 }
 
+$NSREF = seek_nstates($REF_FILE);
+$NSTEST = seek_nstates($TEST_FILE);
+
+$LABEL = "Number of States";
+if($NSREF != $NSTEST) {
+  printf "%-70s...FAILED\n",$LABEL;
+  $FAIL = 1;
+  exit;
+}
+else {
+  printf "%-70s...PASSED\n",$LABEL;
+  $NSTATES = $NSREF;
+}
+    
 @int_ref = seek_excitation_energy($REF_FILE,"OS       RS",$NSTATES);
 @int_test = seek_excitation_energy($TEST_FILE,"OS       RS",$NSTATES);
 
 $LABEL = "Excitation Energy";
-if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, 1, $TOL)) {
+if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, $TOL)) {
   printf "%-70s...FAILED\n",$LABEL;
   $FAIL = 1;
 }
@@ -55,7 +66,7 @@ else {
 @int_test = seek_osc_str($TEST_FILE,"OS       RS",$NSTATES);
 
 $LABEL = "Oscillator Strength";
-if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, 1, $TTOL)) {
+if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, $TTOL)) {
   printf "%-70s...FAILED\n",$LABEL;
   $FAIL = 1;
 }
@@ -67,7 +78,7 @@ else {
 @int_test = seek_rot_str($TEST_FILE,"OS       RS",$NSTATES);
 
 $LABEL = "Rotational Strength";
-if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, 1, $TTOL)) {
+if(!compare_arrays(\@int_ref, \@int_test, $NSTATES, $TTOL)) {
   printf "%-70s...FAILED\n",$LABEL;
   $FAIL = 1;
 }
@@ -76,8 +87,6 @@ else {
 }
     
 close(RE);
-
-system("cat $RESULT");
 
 exit($FAIL);
 
