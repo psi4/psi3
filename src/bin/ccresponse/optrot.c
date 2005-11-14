0335092169df6a4320dec6d566bc3cf55bc832ef
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-11-14 17:30:00 +0000

    Some fixes to simultaneous length/velocity calcs.  -TDC
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@2992 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccresponse/optrot.c
+++ src/bin/ccresponse/optrot.c
@@ -176,10 +176,10 @@ void optrot(void)
 	      "L", moinfo.l_irreps, -params.omega[i]);
 
     if(!strcmp(params.gauge,"BOTH")) {
-      linresp(tensor_rp[i], -0.5, 0.0, "Mu", moinfo.mu_irreps, -params.omega[i],
-	      "P", moinfo.l_irreps, params.omega[i]);
-      linresp(tensor_rp[i], -0.5, 1.0, "Mu", moinfo.mu_irreps, params.omega[i],
-	      "P", moinfo.l_irreps, -params.omega[i]);
+      linresp(tensor_rp[i], -0.5, 0.0, "P", moinfo.mu_irreps, -params.omega[i],
+	      "Mu", moinfo.l_irreps, params.omega[i]);
+      linresp(tensor_rp[i], -0.5, 1.0, "P", moinfo.mu_irreps, params.omega[i],
+	      "Mu", moinfo.l_irreps, -params.omega[i]);
     }
 
     /* Clean up disk space */
@@ -201,17 +201,17 @@ void optrot(void)
     prefactor *= prefactor;
     prefactor *= 288.0e-30 * _pi * _pi * _na * bohr2a4;
 
-    if (!strcmp(params.wfn,"CC2"))
-      fprintf(outfile, "\n                      CC2 Optical Rotation Tensor:\n");
-    else
-      fprintf(outfile, "\n                      CCSD Optical Rotation Tensor:\n");
-    fprintf(outfile, "  -------------------------------------------------------------------------\n");
-    fprintf(outfile,   "   Evaluated at omega = %8.6f E_h (%6.2f nm, %5.3f eV, %8.2f cm-1)\n", params.omega[i],
-	    (_c*_h*1e9)/(_hartree2J*params.omega[i]), _hartree2ev*params.omega[i],
-	    _hartree2wavenumbers*params.omega[i]);
-    fprintf(outfile, "  -------------------------------------------------------------------------\n");
     if(!strcmp(params.gauge,"LENGTH") || !strcmp(params.gauge,"BOTH")) {
-      fprintf(outfile, "    Length-gauge electric-dipole operator:\n");
+      if (!strcmp(params.wfn,"CC2")) 
+        fprintf(outfile, "\n            CC2 Optical Rotation Tensor (Length Gauge):\n");
+      else if(!strcmp(params.wfn,"CCSD"))
+    	fprintf(outfile, "\n           CCSD Optical Rotation Tensor (Length Gauge):\n");
+
+      fprintf(outfile, "  -------------------------------------------------------------------------\n");
+      fprintf(outfile,   "   Evaluated at omega = %8.6f E_h (%6.2f nm, %5.3f eV, %8.2f cm-1)\n", params.omega[i],
+	      (_c*_h*1e9)/(_hartree2J*params.omega[i]), _hartree2ev*params.omega[i],
+	      _hartree2wavenumbers*params.omega[i]);
+      fprintf(outfile, "  -------------------------------------------------------------------------\n");
       mat_print(tensor_rl[i], 3, 3, outfile);
 
       TrG_rl = (tensor_rl[i][0][0] + tensor_rl[i][1][1] + tensor_rl[i][2][2])/(3.0 * params.omega[i]);
@@ -222,8 +222,17 @@ void optrot(void)
     }
 
     if(!strcmp(params.gauge,"VELOCITY") || !strcmp(params.gauge,"BOTH")) {
+
+      if (!strcmp(params.wfn,"CC2")) 
+        fprintf(outfile, "\n          CC2 Optical Rotation Tensor (Velocity Gauge):\n");
+      else if(!strcmp(params.wfn,"CCSD"))
+    	fprintf(outfile, "\n         CCSD Optical Rotation Tensor (Velocity Gauge):\n");
+
       fprintf(outfile, "  -------------------------------------------------------------------------\n");
-      fprintf(outfile, "\n    Velocity-gauge electric-dipole operator:\n");
+      fprintf(outfile,   "   Evaluated at omega = %8.6f E_h (%6.2f nm, %5.3f eV, %8.2f cm-1)\n", params.omega[i],
+	      (_c*_h*1e9)/(_hartree2J*params.omega[i]), _hartree2ev*params.omega[i],
+	      _hartree2wavenumbers*params.omega[i]);
+      fprintf(outfile, "  -------------------------------------------------------------------------\n");
       mat_print(tensor_pl[i], 3, 3, outfile);
 
       TrG_pl = (tensor_pl[i][0][0] + tensor_pl[i][1][1] + tensor_pl[i][2][2])/(3.0 * params.omega[i]);
@@ -232,9 +241,6 @@ void optrot(void)
       rotation_pl[i] = prefactor * TrG_pl * nu * nu / M;
       fprintf(outfile, "\n   Specific rotation using velocity-gauge electric-dipole Rosenfeld tensor.\n");
       fprintf(outfile, "\t[alpha]_(%5.3f) = %20.12f deg/[dm (gm/cm^3)]\n", params.omega[i], rotation_pl[i]);
-    }
-
-    if(!strcmp(params.gauge,"VELOCITY") || !strcmp(params.gauge,"BOTH")) {
 
       /* subtract the zero-frequency beta tensor */
       for(j=0; j < 3; j++)
@@ -243,8 +249,9 @@ void optrot(void)
 
       if (!strcmp(params.wfn,"CC2"))
 	fprintf(outfile, "\n        CC2 Modified Velocity-Gauge Optical Rotation Tensor:\n");
-      else
+      else if(!strcmp(params.wfn,"CCSD"))
 	fprintf(outfile, "\n        CCSD Modified Velocity-Gauge Optical Rotation Tensor:\n");
+
       fprintf(outfile, "  -------------------------------------------------------------------------\n");
       fprintf(outfile,   "   Evaluated at omega = %8.6f E_h (%6.2f nm, %5.3f eV, %8.2f cm-1)\n", params.omega[i],
 	      (_c*_h*1e9)/(_hartree2J*params.omega[i]), _hartree2ev*params.omega[i],
@@ -262,13 +269,18 @@ void optrot(void)
     }
 
     if(!strcmp(params.gauge,"BOTH")) {
+      fprintf(outfile, "Prefactor = %20.12f\n", prefactor);
+      fprintf(outfile, "nu = %20.12f\n", nu);
+      fprintf(outfile, "M = %20.12f\n", M);
+      fprintf(outfile, "<<p;r>>w = \n");
+      mat_print(tensor_rp[i], 3, 3, outfile);
       delta[i][0] = prefactor * (tensor_rp[i][1][2] - tensor_rp[i][2][1]) * nu * nu / M;
       delta[i][1] = prefactor * (tensor_rp[i][2][0] - tensor_rp[i][0][2]) * nu * nu / M;
       delta[i][2] = prefactor * (tensor_rp[i][0][1] - tensor_rp[i][1][0]) * nu * nu / M;
       delta[i][0] /= 3.0 * params.omega[i];
       delta[i][1] /= 3.0 * params.omega[i];
       delta[i][2] /= 3.0 * params.omega[i];
-      fprintf(outfile, "\n   Origin-shift vector for length-gauge rotation deg/[dm (g/cm^3)].\n");
+      fprintf(outfile, "\n   Origin-dependence vector for length-gauge rotation deg/[dm (g/cm^3)].\n");
       fprintf(outfile, "     Delta_x = %6.2f   Delta_y = %6.2f   Delta_z = %6.2f\n",
 	      delta[i][0], delta[i][1], delta[i][2]);
     }
@@ -287,7 +299,7 @@ void optrot(void)
       fprintf(outfile,   "\t E_h      nm   deg/[dm (gm/cm^3)]\n");
       fprintf(outfile,   "\t-----   ------ ------------------\n");
       for(i=0; i < params.nomega; i++)
-        fprintf(outfile, "\t%5.3f   %6.2f      %10.5f\n", params.omega[i], (_c*_h*1e9)/(_hartree2J*params.omega[i]), rotation_rl[i]);
+	fprintf(outfile, "\t%5.3f   %6.2f      %10.5f\n", params.omega[i], (_c*_h*1e9)/(_hartree2J*params.omega[i]), rotation_rl[i]);
     }
 
     if(!strcmp(params.gauge,"VELOCITY") || !strcmp(params.gauge,"BOTH")) {
