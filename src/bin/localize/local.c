ba6ee4b19c93db67646c1c23c8550138b4d20e87
nruss <nruss@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2005-11-17 17:57:09 +0000

    Rechecked in old version for 05/05/22 since new version seg faults. -NJR
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@3010 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/localize/local.c
+++ src/bin/localize/local.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <libipv1/ip_lib.h>
 #include <libciomr/libciomr.h>
 #include <libint/libint.h>
 #include <libchkpt/chkpt.h>
@@ -15,97 +14,10 @@ char *psi_file_prefix;
 void init_io(int argc, char *argv[]);
 void exit_io(void);
 void title(void);
-void pipek_mezey(void);
-void boys(int NAO, int NMO, int first_mo, int last_mo, double **CC);
-
-
-#define METHOD_PIPEK_MEZEY 0
-#define METHOD_BOYS 1
-
 
 int main(int argc, char *argv[])
 {
-  int method_code = METHOD_PIPEK_MEZEY;
-  int localize_virtuals = 0;
-  char *method_string;
-  int errcod, i, nirreps, nmo, nao, nocc, nfzc;
-  int *clsdpi, *openpi, *frdocc;
-  double **C;
-
-  init_io(argc, argv);
-  title();
-
-  errcod = ip_string("LOCALIZATION_METHOD",&method_string,0);
-  if (errcod == IPE_OK) {
-    if (strcmp(method_string,"PIPEK_MEZEY")==0) {
-      method_code = METHOD_PIPEK_MEZEY;
-    }
-    else if (strcmp(method_string,"BOYS")==0) {
-      method_code = METHOD_BOYS;
-    }
-    else {
-      fprintf(outfile, "Error: unrecognized LOCALIZATION_METHOD %s\n",
-        method_string);
-      exit(PSI_RETURN_FAILURE);
-    }
-  }
-
-  /* this option only works with Boys at the moment */
-  errcod = ip_boolean("LOCALIZE_VIRTUALS",&localize_virtuals,0);
-
-  /* Pipek-Mezey localization */
-  if (method_code == METHOD_PIPEK_MEZEY)
-    pipek_mezey();
-
-  /* Boys localization */
-  else if (method_code == METHOD_BOYS) {
-    chkpt_init(PSIO_OPEN_OLD);
-    nao = chkpt_rd_nao();
-    nmo = chkpt_rd_nmo();
-    nirreps = chkpt_rd_nirreps();
-    if(nirreps != 1) {
-      fprintf(outfile, 
-        "\n\tError: localization is only valid in C1 symmetry!\n");
-      exit(PSI_RETURN_FAILURE);
-    }
-    clsdpi = chkpt_rd_clsdpi();
-    openpi = chkpt_rd_openpi();
-    for (i=0,nocc=0; i<nirreps; i++) nocc += clsdpi[i] + openpi[i];
-    free(clsdpi);
-    free(openpi);
-    C = chkpt_rd_scf();
-    
-    /* localize the occupied orbitals (- frozen core) */
-    frdocc = get_frzcpi();
-    nfzc = frdocc[0];
-    fprintf(outfile, "%d core orbitals\n", nfzc);
-    free(frdocc);
-    boys(nao, nmo, nfzc, nocc, C);
-
-    /* if requested, attempt to localize the virtuals */
-    if (localize_virtuals)
-      boys(nao, nmo, nocc, nmo, C);
-
-    chkpt_wt_scf(C);
-    chkpt_close();
-  }
-
-  /* unrecognized localization option */
-  else {
-    fprintf(outfile, "Error: unrecognized METHOD_CODE %d\n", method_code);  
-    exit(PSI_RETURN_FAILURE);
-  } 
-
-  free(method_string);
-  exit_io();
-  exit(PSI_RETURN_SUCCESS);
-}
-
-
-void pipek_mezey(void)
-{
-  int iter, s, t, A, k, l, m, p, q, inew, iold, max, max_col;
-  int phase_ok, phase_chk;
+  int iter, s, t, A, k, l, m, p, q, inew, iold, max, max_col, phase_ok, phase_chk;
   int i, j, ij, am, atom, shell_length, offset, stat;
   int nirreps, nao, nmo, nso, natom, nshell, noei, nocc, errcod, nfzc;
   int *stype, *snuc, *aostart, *aostop, *ao2atom, *l_length;
@@ -119,6 +31,9 @@ void pipek_mezey(void)
   int print;
 
   alphalast = 1.0;
+
+  init_io(argc, argv);
+  title();
 
   chkpt_init(PSIO_OPEN_OLD);
   nao = chkpt_rd_nao();
@@ -431,264 +346,12 @@ void pipek_mezey(void)
 
   free_block(C);
   free(evals);
-  free(clsdpi);
-  free(openpi);
-  free(orbspi);
 
   fprintf(outfile, "\n\tLocalization of occupied orbitals complete.\n");
 
+  exit_io();
+  exit(PSI_RETURN_SUCCESS);
 }
-
-
-void boys(int NAO,int NMO,int first,int last,double **CC)
-{
-  int mu,i,j,ij,k,l,count;
-  int iDmax,jDmax;
-  double *dmxAOarray;
-  double *dmyAOarray;
-  double *dmzAOarray;
-  double **dmxAO;
-  double **dmyAO;
-  double **dmzAO;
-  double **dmxMO;
-  double **dmyMO;
-  double **dmzMO;
-  double *dmMO_diag;
-  double **Ax;
-  double **Ay;
-  double **Az;
-  double **Bx;
-  double **By;
-  double **Bz;
-  double **CC_sorted;
-  double D, DOld=0.0;
-  double Dmax;
-  double nom,denom;
-  double cosa,sina,cos4a,sin4a;
-  double x2plus,x2minus;
-  double x1,x2,y1,y2,sin4acandidate1,sin4acandidate2;
-  double AngleEpsilon,DChangeEpsilon;
-  int boyscountMax = 200;
-  double tval, tval1, tval2;
-  double minD;
-  int minj, *sort_done;
-
-  AngleEpsilon=1.0E-6;
-  DChangeEpsilon=1.0E-6;
-                                                                                
-  fprintf(outfile, "\nPerforming Boys localization for %d AO's and %d MO's\n",
-    NAO, NMO);
-  fprintf(outfile, "Localizing orbitals %d through %d\n", first, last-1);
-
-  fprintf(outfile, "\tIter     D           Conv\n");
-  fprintf(outfile, "\t----------------------------------\n");
-
-  dmxAOarray=init_array(NAO*(NAO+1)/2);
-  dmyAOarray=init_array(NAO*(NAO+1)/2);
-  dmzAOarray=init_array(NAO*(NAO+1)/2);
-
-  iwl_rdone(PSIF_OEI,PSIF_AO_MX,dmxAOarray,NAO*(NAO+1)/2,0,0,outfile);
-  iwl_rdone(PSIF_OEI,PSIF_AO_MY,dmyAOarray,NAO*(NAO+1)/2,0,0,outfile);
-  iwl_rdone(PSIF_OEI,PSIF_AO_MZ,dmzAOarray,NAO*(NAO+1)/2,0,0,outfile);
-                                                                                
-  dmxAO=block_matrix(NAO,NAO);
-  dmyAO=block_matrix(NAO,NAO);
-  dmzAO=block_matrix(NAO,NAO);
-                                                                                
-  for(i=0,ij=0;i<NAO;i++){
-    for(j=0;j<=i;j++,ij++){
-      dmxAO[i][j]=dmxAOarray[ij];
-      dmyAO[i][j]=dmyAOarray[ij];
-      dmzAO[i][j]=dmzAOarray[ij];
-      dmxAO[j][i]=dmxAO[i][j];
-      dmyAO[j][i]=dmyAO[i][j];
-      dmzAO[j][i]=dmzAO[i][j];
-    }
-  }
-                                                                                
-  free(dmxAOarray);
-  free(dmyAOarray);
-  free(dmzAOarray);
-                                                                                
-  dmxMO=block_matrix(NMO,NMO);
-  dmyMO=block_matrix(NMO,NMO);
-  dmzMO=block_matrix(NMO,NMO);
-                                                                                
-  Ax=block_matrix(NMO,NMO);
-  Ay=block_matrix(NMO,NMO);
-  Az=block_matrix(NMO,NMO);
-  Bx=block_matrix(NMO,NMO);
-  By=block_matrix(NMO,NMO);
-  Bz=block_matrix(NMO,NMO);
-
-  if (first >= last) return;
-  
-  for(count=0;count<boyscountMax;count++){
-    for(i=first;i<last;i++){
-      for(j=first;j<last;j++){
-        dmxMO[i][j]=0.0;
-        dmyMO[i][j]=0.0;
-        dmzMO[i][j]=0.0;
-        for(k=0;k<NAO;k++){
-          for(l=0;l<NAO;l++){
-            dmxMO[i][j]+=CC[k][i]*CC[l][j]*dmxAO[k][l];
-            dmyMO[i][j]+=CC[k][i]*CC[l][j]*dmxAO[k][l];
-            dmzMO[i][j]+=CC[k][i]*CC[l][j]*dmxAO[k][l];
-          }
-        }
-      }
-    }
-                                                                                
-    D=0.0;
-    for(i=first;i<last;i++){
-      D+=dmxMO[i][i]*dmxMO[i][i]+dmyMO[i][i]*dmyMO[i][i]+
-        dmzMO[i][i]*dmzMO[i][i];
-    }
-                                                                                
-    fprintf(outfile,"\t%3d  %12.6lf    %2.2E\n",count,D,fabs(D-DOld));
-    if(count!=0){
-      if(fabs(D-DOld)<DChangeEpsilon){
-        fprintf(outfile,"\nConvergence to %2.2E is achieved.\n",
-          count,DChangeEpsilon);
-        break;
-      }
-    }
-                                                                                
-    DOld=D;
-                                                                                
-    for(i=first;i<last;i++){
-      for(j=first;j<last;j++){
-        tval = dmxMO[i][i]-dmxMO[j][j];
-        Ax[i][j]=dmxMO[i][j]*dmxMO[i][j]-0.25*tval*tval;
-        Bx[i][j]=dmxMO[i][j]*tval;
-        tval = dmyMO[i][i]-dmyMO[j][j];
-        Ay[i][j]=dmyMO[i][j]*dmyMO[i][j]-0.25*tval*tval;
-        By[i][j]=dmyMO[i][j]*tval;
-        tval = dmzMO[i][i]-dmzMO[j][j];
-        Az[i][j]=dmzMO[i][j]*dmzMO[i][j]-0.25*tval*tval;
-        Bz[i][j]=dmzMO[i][j]*tval;
-      }
-    }
-                                                                                
-    Dmax=0.0;
-    iDmax=1;
-    jDmax=1;
-    for(i=first;i<last;i++){
-      for(j=first;j<i;j++){
-        D=Ax[i][j]+Ay[i][j]+Az[i][j]+
-          sqrt(Ax[i][j]*Ax[i][j]+Bx[i][j]*Bx[i][j])+
-          sqrt(Ay[i][j]*Ay[i][j]+By[i][j]*By[i][j])+
-          sqrt(Az[i][j]*Az[i][j]+Bz[i][j]*Bz[i][j]);
-        if(D>Dmax){
-          Dmax=D;
-          iDmax=i;
-          jDmax=j;
-        }
-      };
-    };
-                                                                                
-    /*printf("Dmax occupied  = %lf\n",Dmax);*/
-                                                                                
-    nom=Ax[iDmax][jDmax]+Ay[iDmax][jDmax]+Az[iDmax][jDmax];
-    denom=sqrt(Ax[iDmax][jDmax]*Ax[iDmax][jDmax]+
-               Bx[iDmax][jDmax]*Bx[iDmax][jDmax])
-      +sqrt(Ay[iDmax][jDmax]*Ay[iDmax][jDmax]+
-           By[iDmax][jDmax]*By[iDmax][jDmax])+
-      +sqrt(Az[iDmax][jDmax]*Az[iDmax][jDmax]+
-            Bz[iDmax][jDmax]*Bz[iDmax][jDmax]);
-    cos4a=-nom/denom;
-                                                                                
-    nom=Bx[iDmax][jDmax]+By[iDmax][jDmax]+Bz[iDmax][jDmax];
-    sin4a=nom/denom;
-                                                                                
-    x2plus=0.5*(1.0+sqrt((1.0-0.5*(1.0-cos4a))));
-    x2minus=0.5*(1.0-sqrt((1.0-0.5*(1.0-cos4a))));
-                                                                                
-    x1=sqrt(x2plus);
-    x2=sqrt(x2minus);
-                                                                                
-    y1=sqrt(1-x2plus);
-    y2=sqrt(1-x2minus);
-                                                                                
-    sin4acandidate1=4.0*x1*y1*(x1*x1-y1*y1);
-    sin4acandidate2=4.0*x2*y2*(x2*x2-y2*y2);
-                                                                                
-    /*printf("sin4a occupied = %lf\n",sin4a);
-    printf("sin4acandidate1 occupied = %lf\n",sin4acandidate1);
-    printf("sin4acandidate2 occupied = %lf\n",sin4acandidate2);*/
-                                                                                
-    if (fabs(sin4acandidate1-sin4a)<AngleEpsilon){
-      cosa=x1;
-      sina=y1;
-      /*printf("cos(a) occupied = %lf\n",cosa);
-        printf("sin(a) occupied = %lf\n",sina);*/
-    }
-    else{
-      if (fabs(sin4acandidate2-sin4a)<AngleEpsilon){
-        cosa=x2;
-        sina=y2;
-        /*printf("cos(a) occupied = %lf\n",cosa);
-          printf("sin(a) occupied = %lf\n",sina);*/
-      }
-      else{
-        printf("Cannot find the rotation angle in occupieds!\n");
-      }
-    }
-                                                                                
-    for(mu=0;mu<NAO;mu++){
-      tval1 =  cosa*CC[mu][iDmax]+sina*CC[mu][jDmax];
-      tval2 = -sina*CC[mu][iDmax]+cosa*CC[mu][jDmax];
-      CC[mu][iDmax] = tval1;
-      CC[mu][jDmax] = tval2;
-    }
-                                                                                
-  } /* end boys iterations */
-
-  /* now sort the MO's according to the size of dm_MO[] */
-  CC_sorted = block_matrix(NAO, last-first+1);
-  dmMO_diag = init_array(last-first);
-  for (i=first; i<last; i++) {
-    dmMO_diag[i-first] = dmxMO[i][i]*dmxMO[i][i] 
-                        + dmyMO[i][i]*dmyMO[i][i]
-                        + dmzMO[i][i]*dmzMO[i][i];
-
-  }
-
-  sort_done = init_int_array(last-first);
-
-  for (i=0; i<last-first; i++) {
-
-    /* find the next biggest element of dmMO_diag */
-    minD = 100000.0;
-    minj = 0;
-    for (j=0; j<last-first; j++) {
-      if ((dmMO_diag[j] <= minD) && !sort_done[j]) {
-        minD = dmMO_diag[j];
-        minj = j;
-      }
-    }
-    sort_done[minj] = 1;
-    for (k=0; k<NAO; k++) {
-      CC_sorted[k][i] = CC[k][minj+first]; 
-    }
-  }
-
-  for (i=0; i<NAO; i++) {
-    for (j=first,k=0; j<last; j++,k++) {
-      CC[i][j] = CC_sorted[i][k];
-    }
-  }
-
-  free_block(dmxAO);  free_block(dmyAO);  free_block(dmzAO);
-  free_block(dmxMO);  free_block(dmyMO);  free_block(dmzMO);
-  free_block(Ax);     free_block(Ay);     free_block(Az);
-  free_block(Bx);     free_block(By);     free_block(Bz);
-  free(dmMO_diag);    free(sort_done);
-  free_block(CC_sorted);
-}
-
-
-
 
 void init_io(int argc, char * argv[])
 {
@@ -712,7 +375,7 @@ void title(void)
   fprintf(outfile, "\n");
   fprintf(outfile, "\t\t\t**************************\n");
   fprintf(outfile, "\t\t\t*                        *\n");
-  fprintf(outfile, "\t\t\t*         LOCALIZE       *\n");
+  fprintf(outfile, "\t\t\t*          LOCAL         *\n");
   fprintf(outfile, "\t\t\t*                        *\n");
   fprintf(outfile, "\t\t\t**************************\n");
   fprintf(outfile, "\n");
@@ -727,7 +390,7 @@ void exit_io(void)
 
 char *gprgid()
 {
-   char *prgid = "LOCALIZE";
+   char *prgid = "LOCAL";
 
    return(prgid);
 }
