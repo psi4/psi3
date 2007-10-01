d9da8018db0e73635d3ce6ddc94b69a023469dc2
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2007-10-01 18:56:59 +0000

    Test before opening files if any 2 will have identical names.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@3626 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libpsio/open.cc
+++ src/lib/libpsio/open.cc
@@ -10,6 +10,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string>
+#include <map>
+#include <sstream>
 #include <libpsio/psio.h>
 #include <libpsio/psio.hpp>
 
@@ -27,20 +30,39 @@ void PSIO::open(unsigned int unit, int status) {
   
   this_unit = &(psio_unit[unit]);
   
-  /* Check to see if this unit is aleady open */
-  if (this_unit->vol[0].stream != -1)
-    psio_error(unit, PSIO_ERROR_REOPEN);
-  
   /* Get number of volumes to stripe across */
   this_unit->numvols = get_numvols(unit);
   if (this_unit->numvols > PSIO_MAXVOL)
     psio_error(unit, PSIO_ERROR_MAXVOL);
-  
   if (!(this_unit->numvols))
     this_unit->numvols = 1;
   
+  /* Check to see if this unit is already open */
+  for (i=0; i < this_unit->numvols; i++) {
+    if (this_unit->vol[i].stream != -1)
+      psio_error(unit, PSIO_ERROR_REOPEN);
+  }
+  
   /* Get the file name prefix */
   get_filename(unit, &name);
+
+  // Check if any files will have the same name
+  {
+    using std::string;
+    typedef std::map<string,int> Names;
+    Names names;
+    for (i=0; i < this_unit->numvols; i++) {
+      std::ostringstream oss;
+      get_volpath(unit, i, &path);
+      oss << path << name << "." << unit;
+      const std::string fullpath = oss.str();
+      typedef Names::const_iterator citer;
+      citer n = names.find(fullpath);
+      if (n != names.end())
+        psio_error(unit, PSIO_ERROR_IDENTVOLPATH);
+      names[fullpath] = 1;
+    }
+  }
   
   /* Build the name for each volume and open the file */
   for (i=0; i < this_unit->numvols; i++) {
@@ -52,14 +74,9 @@ void PSIO::open(unsigned int unit, int status) {
     this_unit->vol[i].path = strdup(fullpath);
     free(fullpath);
     
-    /* Check if any previously opened volumes have the same path */
-    for (j=0; j < i; j++)
-      if (!strcmp(this_unit->vol[i].path, this_unit->vol[j].path))
-        psio_error(unit, PSIO_ERROR_IDENTVOLPATH);
-    
     /* Now open the volume */
     if (status == PSIO_OPEN_OLD) {
-      this_unit->vol[i].stream =::open(this_unit->vol[i].path,O_CREAT|O_RDWR,0644);
+      this_unit->vol[i].stream = ::open(this_unit->vol[i].path,O_CREAT|O_RDWR,0644);
       if(this_unit->vol[i].stream == -1)
       psio_error(unit,PSIO_ERROR_OPEN);
     }
