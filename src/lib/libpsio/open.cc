e8be0f8bb19cfb85c4caff5e831eb2426c27b4a3
crawdad <crawdad@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2009-02-15 21:29:23 +0000

    copying changes from psi-3-4 release branch to trunk: src/lib/libpsio
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@4103 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/lib/libpsio/open.cc
+++ src/lib/libpsio/open.cc
@@ -4,9 +4,6 @@
  */
 
 #include <cstdio>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <cstring>
 #include <cstdlib>
 #include <unistd.h>
@@ -15,6 +12,7 @@
 #include <sstream>
 #include <libpsio/psio.h>
 #include <libpsio/psio.hpp>
+#include <libpsio/workaround.hpp>
 
 using namespace psi;
 
@@ -23,26 +21,26 @@ void PSIO::open(unsigned int unit, int status) {
   int stream;
   char *name, *path;
   psio_ud *this_unit;
-  
+
   /* check for too large unit */
   if (unit > PSIO_MAXUNIT)
     psio_error(unit, PSIO_ERROR_MAXUNIT);
-  
+
   this_unit = &(psio_unit[unit]);
-  
+
   /* Get number of volumes to stripe across */
   this_unit->numvols = get_numvols(unit);
   if (this_unit->numvols > PSIO_MAXVOL)
     psio_error(unit, PSIO_ERROR_MAXVOL);
   if (!(this_unit->numvols))
     this_unit->numvols = 1;
-  
+
   /* Check to see if this unit is already open */
   for (i=0; i < this_unit->numvols; i++) {
     if (this_unit->vol[i].stream != -1)
       psio_error(unit, PSIO_ERROR_REOPEN);
   }
-  
+
   /* Get the file name prefix */
   get_filename(unit, &name);
 
@@ -64,26 +62,27 @@ void PSIO::open(unsigned int unit, int status) {
       free(path);
     }
   }
-  
+
   /* Build the name for each volume and open the file */
   for (i=0; i < this_unit->numvols; i++) {
     char* fullpath;
     get_volpath(unit, i, &path);
-    
+
     fullpath = (char*) malloc( (strlen(path)+strlen(name)+80)*sizeof(char));
     sprintf(fullpath, "%s%s.%u", path, name, unit);
     this_unit->vol[i].path = strdup(fullpath);
     free(fullpath);
-    
+
     /* Now open the volume */
     if (status == PSIO_OPEN_OLD) {
-      this_unit->vol[i].stream = ::open(this_unit->vol[i].path,O_CREAT|O_RDWR,0644);
+      // see workaround.hpp
+      this_unit->vol[i].stream = psi::detail::_OpeN_creatrdwd(this_unit->vol[i].path);
       if(this_unit->vol[i].stream == -1)
       psio_error(unit,PSIO_ERROR_OPEN);
     }
     else if(status == PSIO_OPEN_NEW) {
-      this_unit->vol[i].stream =
-      ::open(this_unit->vol[i].path,O_CREAT|O_RDWR|O_TRUNC,0644);
+      // see workaround.hpp
+      this_unit->vol[i].stream = psi::detail::_OpeN_creatrdwdtrunc(this_unit->vol[i].path);
       if(this_unit->vol[i].stream == -1)
       psio_error(unit,PSIO_ERROR_OPEN);
     }
@@ -108,8 +107,8 @@ void
 PSIO::rehash(unsigned int unit)
 {
   if (open_check(unit)) {
-    close(unit,1);
-    open(unit,PSIO_OPEN_OLD);
+    this->close(unit,1);
+    this->open(unit,PSIO_OPEN_OLD);
   }
 }
 
@@ -121,7 +120,7 @@ extern "C" {
    **  \param unit   = The PSI unit number used to identify the file to all
    **                  read and write functions.
    **  \param status = Indicates if the file is old (PSIO_OPEN_OLD) or new
-   **                  (PSIO_OPEN_NEW). 
+   **                  (PSIO_OPEN_NEW).
    **
    ** \ingroup PSIO
    */
