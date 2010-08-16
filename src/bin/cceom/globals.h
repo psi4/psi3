4ecf46cff902208d6e89c2e17bc2d73383d054b1
rking <rking@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2010-08-16 03:29:47 +0000

    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@4378 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/cceom/globals.h
+++ src/bin/cceom/globals.h
@@ -17,8 +17,8 @@ namespace psi { namespace cceom {
 
 EXTERN void check_sum(char *term_lbl, int index, int irrep);
 
-// #define EOM_DEBUG (0)
-#define TIME_CCEOM (1)
+//#define EOM_DEBUG (1)
+//#define TIME_CCEOM (1)
 
 #define H_IRR (0)
 #define MAX(I,J) ((I>J) ? I : J)
