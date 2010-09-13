7b7b009c9212df53fee87aadaa5e8f2451702b67
evaleev <evaleev@29f243b5-bcef-41bb-bbe7-8e20262f3206>
2010-09-13 23:13:42 +0000

    pCCSD works even when wfn=ccsd_t.
    git-svn-id: https://sirius.chem.vt.edu/svn/psi3/trunk@4391 29f243b5-bcef-41bb-bbe7-8e20262f3206

--- src/bin/ccenergy/ccenergy.cc
+++ src/bin/ccenergy/ccenergy.cc
@@ -271,7 +271,7 @@ int main(int argc, char *argv[])
       t2_build();
       if(params.print & 2) status("T2 amplitudes", outfile);
       { // do extra work for pCCSD
-        const bool do_pccsd = !strcmp(params.wfn,"CCSD") && (params.pccsd_alpha != 1.0 || params.pccsd_beta != 1.0);
+        const bool do_pccsd = (!strcmp(params.wfn,"CCSD") || !strcmp(params.wfn,"CCSD_T")) && (params.pccsd_alpha != 1.0 || params.pccsd_beta != 1.0);
         if (do_pccsd) {
           t2_delta_pCCSD_build();
           if(params.print & 2) status("delta(T2) pCCSD amplitudes", outfile);
